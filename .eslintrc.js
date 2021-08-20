46a476a7f6197fd42598ecd604cdc4279bf7abce
John Goh <john@flanksource.com>
2021-08-20 14:13:42 +0800

    fix: enable Prettier in .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,18 +2,18 @@ module.exports = {
   env: {
     browser: true,
     commonjs: true,
-    es2020: true
+    es2020: true,
   },
   root: true,
-  // extends: ["eslint:recommended", "plugin:react/recommended", "prettier"],
-  // plugins: ["prettier"],
+  extends: ["eslint:recommended", "plugin:react/recommended", "prettier"],
+  plugins: ["prettier"],
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: "module"
+    sourceType: "module",
   },
   rules: {
     // "prettier/prettier": ["off"],
     "react/react-in-jsx-scope": "off",
-    "react/prop-types": "off"
-  }
+    "react/prop-types": "off",
+  },
 };
