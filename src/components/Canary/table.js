a30c669d81b8dc2cfee818ecea10c57b57f41604
John Goh <john@flanksource.com>
2021-08-20 19:29:45 +0800

    fix: revert another unintended change (canary table item name)

--- src/components/Canary/table.js
+++ src/components/Canary/table.js
@@ -32,7 +32,7 @@ export function CanaryTable({ ...props }) {
               className="cursor-pointer"
             >
               <td className="px-6 py-2 w-full max-w-0 overflow-hidden overflow-ellipsis whitespace-nowrap">
-                asdas dasda sasd asd asd as d
+                <Title check={check} />
               </td>
               <td className="px-6 py-2 whitespace-nowrap">
                 <StatusList check={check} />
