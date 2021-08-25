00d308200c370f7e3ee0c0b465bced5dae2ce963
John Goh <john@flanksource.com>
2021-08-25 13:29:04 +0800

    fix: resolve fixable linting errors

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,8 +1,8 @@
 import {
   QuestionMarkCircleIcon,
-  SortAscendingIcon,
-  UsersIcon
+  SortAscendingIcon
 } from "@heroicons/react/solid";
+import { HiOutlineUsers } from "react-icons/hi";
 import { Icon } from "../Icon";
 
 /*
@@ -290,7 +290,10 @@ export function Example3() {
       <div className="mt-1 flex rounded-md shadow-sm">
         <div className="relative flex items-stretch flex-grow focus-within:z-10">
           <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-            <UsersIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+            <HiOutlineUsers
+              className="h-5 w-5 text-gray-400"
+              aria-hidden="true"
+            />
           </div>
           <input
             type="text"
