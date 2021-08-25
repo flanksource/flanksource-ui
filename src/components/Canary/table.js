013799cd289dbe6bcda60a91c5850d1445c76326
John Goh <john@flanksource.com>
2021-08-26 02:14:03 +0800

    feat: Collapsible table rows for groupings

--- src/components/Canary/table.js
+++ src/components/Canary/table.js
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { FaChevronRight } from "react-icons/fa";
 import { Title, Uptime, Latency } from "./data";
 import { StatusList } from "./status";
 
-export function CanaryTable({ className, theadClass, ...props }) {
+export function CanaryTable({
+  className,
+  hasGrouping,
+  groupingLabel,
+  checks,
+  onClick,
+  theadClass,
+  ...props
+}) {
   const tableHeaderClass = `px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider bg-gray-100 first:rounded-tl-md last:rounded-tr-md`;
   return (
     <div className={`rounded-md border border-gray-200 ${className}`}>
@@ -10,7 +19,7 @@ export function CanaryTable({ className, theadClass, ...props }) {
         <thead className={theadClass}>
           <tr>
             <th scope="col" className={tableHeaderClass}>
-              Check
+              {hasGrouping ? groupingLabel : "Check"}
             </th>
             <th scope="col" className={tableHeaderClass}>
               Health
@@ -25,28 +34,103 @@ export function CanaryTable({ className, theadClass, ...props }) {
           </tr>
         </thead>
         <tbody className="bg-white divide-y divide-gray-200">
-          {props.checks.map((check) => (
-            <tr
-              key={check.key}
-              onClick={() => props.onClick(check)}
-              className="cursor-pointer"
-            >
-              <td className="px-6 py-2 w-full max-w-0 overflow-hidden overflow-ellipsis whitespace-nowrap">
-                <Title check={check} />
-              </td>
-              <td className="px-6 py-2 whitespace-nowrap">
-                <StatusList check={check} />
-              </td>
-              <td className="px-6 py-2 whitespace-nowrap">
-                <Uptime check={check} />
-              </td>
-              <td className="px-6 py-2 whitespace-nowrap">
-                <Latency check={check} />
-              </td>
-            </tr>
-          ))}
+          {hasGrouping ? (
+            <>
+              {Object.keys(checks)
+                .sort((a, b) => {
+                  if (b === "Others") {
+                    return -1;
+                  }
+                  return 1;
+                })
+                .map((group) => (
+                  <TableGroupRow
+                    key={group}
+                    title={group}
+                    items={checks[group]}
+                    onClick={onClick}
+                  />
+                ))}
+            </>
+          ) : (
+            <>
+              {checks.map((check) => (
+                <tr
+                  key={check.key}
+                  onClick={() => onClick(check)}
+                  className="cursor-pointer"
+                >
+                  <td className="px-6 py-2 w-full max-w-0 overflow-hidden overflow-ellipsis whitespace-nowrap">
+                    <Title check={check} />
+                  </td>
+                  <td className="px-6 py-2 whitespace-nowrap">
+                    <StatusList check={check} />
+                  </td>
+                  <td className="px-6 py-2 whitespace-nowrap">
+                    <Uptime check={check} />
+                  </td>
+                  <td className="px-6 py-2 whitespace-nowrap">
+                    <Latency check={check} />
+                  </td>
+                </tr>
+              ))}
+            </>
+          )}
         </tbody>
       </table>
     </div>
+  );
+}
+
+function TableGroupRow({ title, items, onClick, ...props }) {
+  const [expanded, setExpanded] = useState(false);
+
+  return (
+    <>
+      <tr
+        title="Click to expand row"
+        aria-expanded={expanded}
+        className="cursor-pointer bg-white"
+        onClick={() => {
+          setExpanded(!expanded);
+        }}
+        {...props}
+      >
+        <td className="px-6 py-3 w-full">
+          <div className="flex items-center select-none">
+            <FaChevronRight
+              className={`${
+                expanded ? "transform rotate-90" : ""
+              } mr-2 duration-75`}
+            />
+            {title}
+          </div>
+        </td>
+        <td />
+        <td />
+        <td />
+      </tr>
+      {expanded &&
+        items.map((item) => (
+          <tr
+            key={item.key}
+            onClick={() => onClick(item)}
+            className="cursor-pointer "
+          >
+            <td className="px-6 pl-12 py-2 w-full max-w-0 overflow-hidden overflow-ellipsis whitespace-nowrap">
+              <Title check={item} />
+            </td>
+            <td className="px-6 py-2 whitespace-nowrap">
+              <StatusList check={item} />
+            </td>
+            <td className="px-6 py-2 whitespace-nowrap">
+              <Uptime check={item} />
+            </td>
+            <td className="px-6 py-2 whitespace-nowrap">
+              <Latency check={item} />
+            </td>
+          </tr>
+        ))}
+    </>
   );
 }
