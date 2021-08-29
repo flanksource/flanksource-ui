c3a9d8c09df642a635207845e43a9a9fc4f931e9
Moshe Immerman <moshe@flanksource.com>
2021-08-29 16:11:13 +0200

    fix: example pages

--- src/pages/Examples/canary.js
+++ src/pages/Examples/canary.js
@@ -1,9 +1,9 @@
-import { Canary } from "../../components/Canary";
+import Canary from "../../components/Canary";
 
 export function CanaryPage() {
   return (
     <div className="max-w-screen-xl mx-auto flex justify-center">
-      <Canary url="http://localhost:8080/api" />
+      <Canary url="/api" />
     </div>
   );
 }
