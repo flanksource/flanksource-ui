c29845a4fe9e2e1d145aa41a9bfe2414b4c41ecc
Moshe Immerman <moshe@flanksource.com>
2021-08-29 16:06:06 +0200

    chore: add mobx

--- craco.config.js
+++ craco.config.js
@@ -1,5 +1,10 @@
 // craco.config.js
 module.exports = {
+
+  eslint: {
+    enable: false
+  },
+
   style: {
     postcss: {
       // eslint-disable-next-line global-require, import/no-extraneous-dependencies
