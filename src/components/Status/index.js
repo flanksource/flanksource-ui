af97de812e649b1c859070515f4fab6303c74859
Moshe Immerman <moshe@flanksource.com>
2021-08-29 16:01:22 +0200

    fix: add mixed style

--- src/components/Status/index.js
+++ src/components/Status/index.js
@@ -1,14 +1,15 @@
-export function Status({ state, className = "" }) {
-  if (state === "ok") {
+import "./style.css"
+export function Status({ good, mixed, className = "" }) {
+  if (mixed) {
+    className += " bg-mixed";
+  } else if (good) {
     className += " bg-green-400";
-  } else if (state === "bad") {
-    className += " bg-red-400";
   } else {
-    className += " bg-gray-400";
+    className += " bg-red-400";
   }
   return (
     <span
-      className={`${className} flex-shrink-0 inline-block h-2 w-2 rounded-full shadow-md`}
+      className={`${className} flex-shrink-0 inline-block h-3 w-3 rounded-full shadow-md`}
       aria-hidden="true"
     />
   );
