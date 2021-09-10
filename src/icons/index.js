674eebca99bc05558c89028513c38692b8990ce7
Moshe Immerman <moshe@flanksource.com>
2021-09-10 16:23:55 +0200

    feat: add more icons

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,19 +1,42 @@
 // this file is generated using make index.js
+import acr from "./acr.svg";
+import aks from "./aks.svg";
 import alert from "./alert.svg";
+import apiconnect from "./apiconnect.svg";
 import aws from "./aws.svg";
+import azure_acl from "./azure-acl.svg";
+import azure_app-service from "./azure-app-service.svg";
+import azure_devops from "./azure-devops.svg";
+import azure_lb from "./azure-lb.svg";
+import azure_log-analytics from "./azure-log-analytics.svg";
+import azure_metrics from "./azure-metrics.svg";
+import azure_service-bus from "./azure-service-bus.svg";
+import azure_sql-server from "./azure-sql-server.svg";
+import azure_sql from "./azure-sql.svg";
+import azure_subscription from "./azure-subscription.svg";
+import azure_vnet from "./azure-vnet.svg";
 import azure from "./azure.svg";
 import bash from "./bash.svg";
 import ca from "./ca.svg";
-import canaryChecker from "./canary-checker.svg";
+import canary_checker from "./canary-checker.svg";
 import cassandra from "./cassandra.svg";
 import check from "./check.svg";
 import cisco from "./cisco.svg";
+import cloud_build from "./cloud-build.svg";
 import cloudformation from "./cloudformation.svg";
 import cloudsql from "./cloudsql.svg";
+import cloudwatch_alarm from "./cloudwatch-alarm.svg";
+import cloudwatch_config from "./cloudwatch-config.svg";
+import cloudwatch from "./cloudwatch.svg";
+import compute_engine from "./compute-engine.svg";
 import consul from "./consul.svg";
 import containerdPull from "./containerdPull.svg";
 import containerdPush from "./containerdPush.svg";
 import datadog from "./datadog.svg";
+import datapower_monitoring from "./datapower-monitoring.svg";
+import datapower_routing from "./datapower-routing.svg";
+import datapower_threat from "./datapower-threat.svg";
+import datapower from "./datapower.svg";
 import dex from "./dex.svg";
 import django from "./django.svg";
 import dns from "./dns.svg";
@@ -32,10 +55,14 @@ import etcd from "./etcd.svg";
 import fire from "./fire.svg";
 import fluentd from "./fluentd.svg";
 import flux from "./flux.svg";
+import folder from "./folder.svg";
+import gcp_repos from "./gcp-repos.svg";
 import gcp from "./gcp.svg";
+import gcsBucket from "./gcsBucket.svg";
 import gilab from "./gilab.svg";
 import git from "./git.svg";
 import github from "./github.svg";
+import gke from "./gke.svg";
 import google from "./google.svg";
 import grafana from "./grafana.svg";
 import graphql from "./graphql.svg";
@@ -48,6 +75,8 @@ import helm from "./helm.svg";
 import html from "./html.svg";
 import http from "./http.svg";
 import https from "./https.svg";
+import ibm_cloud from "./ibm-cloud.svg";
+import ibm_mq from "./ibm-mq.svg";
 import ibm from "./ibm.svg";
 import icmp from "./icmp.svg";
 import istio from "./istio.svg";
@@ -65,6 +94,7 @@ import memchache from "./memchache.svg";
 import microsoft from "./microsoft.svg";
 import mongo from "./mongo.svg";
 import mssql from "./mssql.svg";
+import multiple from "./multiple.svg";
 import namespace from "./namespace.svg";
 import newrelic from "./newrelic.svg";
 import nginx from "./nginx.svg";
@@ -79,7 +109,6 @@ import postgres from "./postgres.svg";
 import prometheus from "./prometheus.svg";
 import python from "./python.svg";
 import rabbitmq from "./rabbitmq.svg";
-import multiple from "./multiple.svg";
 import rancher from "./rancher.svg";
 import redhat from "./redhat.svg";
 import redis from "./redis.svg";
@@ -90,111 +119,147 @@ import s3Bucket from "./s3Bucket.svg";
 import slack from "./slack.svg";
 import smb from "./smb.svg";
 import spring from "./spring.svg";
+import sql from "./sql.svg";
 import ssl from "./ssl.svg";
+import stackdriver_monitoring from "./stackdriver-monitoring.svg";
+import stackdriver_trace from "./stackdriver-trace.svg";
+import stackdriver from "./stackdriver.svg";
 import terraform from "./terraform.svg";
 import trafficlight from "./trafficlight.svg";
 import ubuntu from "./ubuntu.svg";
 import vault from "./vault.svg";
 import vsphere from "./vsphere.svg";
 import www from "./www.svg";
-
 export const Icons = {
-  alert,
-  aws,
-  azure,
-  bash,
-  ca,
-  "canary-checker": canaryChecker,
-  cassandra,
-  check,
-  cisco,
-  cloudformation,
-  cloudsql,
-  consul,
-  containerdPull,
-  containerdPush,
-  datadog,
-  dex,
-  django,
-  dns,
-  dockerPull,
-  dockerPush,
-  dotnet,
-  dynatrace,
-  ec2,
-  ecr,
-  eks,
-  elastic,
-  elasticsearch,
-  elb,
-  envoy,
-  etcd,
-  fire,
-  fluentd,
-  flux,
-  gcp,
-  gilab,
-  git,
-  github,
-  google,
-  grafana,
-  graphql,
-  grpc,
-  grpc2,
-  haproxy,
-  harbor,
-  hazelcast,
-  helm,
-  html,
-  http,
-  https,
-  ibm,
-  icmp,
-  istio,
-  jaegar,
-  java,
-  jmeter,
-  junit,
-  k8s,
-  kubernetes,
-  ldap,
-  letsencrypt,
-  linux,
-  lua,
-  memchache,
-  microsoft,
-  mongo,
-  mssql,
-  multiple,
-  namespace,
-  newrelic,
-  nginx,
-  nodejs,
-  opa,
-  openshift,
-  oracle,
-  pagerduty,
-  php,
-  pod,
-  postgres,
-  prometheus,
-  python,
-  rabbitmq,
-  rancher,
-  redhat,
-  redis,
-  restic,
-  ruby,
-  s3,
-  s3Bucket,
-  slack,
-  smb,
-  spring,
-  ssl,
-  terraform,
-  trafficlight,
-  ubuntu,
-  vault,
-  vsphere,
-  www
-};
+"acr": acr,
+"aks": aks,
+"alert": alert,
+"apiconnect": apiconnect,
+"aws": aws,
+"azure-acl": azure_acl,
+"azure-app-service": azure_app-service,
+"azure-devops": azure_devops,
+"azure-lb": azure_lb,
+"azure-log-analytics": azure_log-analytics,
+"azure-metrics": azure_metrics,
+"azure-service-bus": azure_service-bus,
+"azure-sql-server": azure_sql-server,
+"azure-sql": azure_sql,
+"azure-subscription": azure_subscription,
+"azure-vnet": azure_vnet,
+"azure": azure,
+"bash": bash,
+"ca": ca,
+"canary-checker": canary_checker,
+"cassandra": cassandra,
+"check": check,
+"cisco": cisco,
+"cloud-build": cloud_build,
+"cloudformation": cloudformation,
+"cloudsql": cloudsql,
+"cloudwatch-alarm": cloudwatch_alarm,
+"cloudwatch-config": cloudwatch_config,
+"cloudwatch": cloudwatch,
+"compute-engine": compute_engine,
+"consul": consul,
+"containerdPull": containerdPull,
+"containerdPush": containerdPush,
+"datadog": datadog,
+"datapower-monitoring": datapower_monitoring,
+"datapower-routing": datapower_routing,
+"datapower-threat": datapower_threat,
+"datapower": datapower,
+"dex": dex,
+"django": django,
+"dns": dns,
+"dockerPull": dockerPull,
+"dockerPush": dockerPush,
+"dotnet": dotnet,
+"dynatrace": dynatrace,
+"ec2": ec2,
+"ecr": ecr,
+"eks": eks,
+"elastic": elastic,
+"elasticsearch": elasticsearch,
+"elb": elb,
+"envoy": envoy,
+"etcd": etcd,
+"fire": fire,
+"fluentd": fluentd,
+"flux": flux,
+"folder": folder,
+"gcp-repos": gcp_repos,
+"gcp": gcp,
+"gcsBucket": gcsBucket,
+"gilab": gilab,
+"git": git,
+"github": github,
+"gke": gke,
+"google": google,
+"grafana": grafana,
+"graphql": graphql,
+"grpc": grpc,
+"grpc2": grpc2,
+"haproxy": haproxy,
+"harbor": harbor,
+"hazelcast": hazelcast,
+"helm": helm,
+"html": html,
+"http": http,
+"https": https,
+"ibm-cloud": ibm_cloud,
+"ibm-mq": ibm_mq,
+"ibm": ibm,
+"icmp": icmp,
+"istio": istio,
+"jaegar": jaegar,
+"java": java,
+"jmeter": jmeter,
+"junit": junit,
+"k8s": k8s,
+"kubernetes": kubernetes,
+"ldap": ldap,
+"letsencrypt": letsencrypt,
+"linux": linux,
+"lua": lua,
+"memchache": memchache,
+"microsoft": microsoft,
+"mongo": mongo,
+"mssql": mssql,
+"multiple": multiple,
+"namespace": namespace,
+"newrelic": newrelic,
+"nginx": nginx,
+"nodejs": nodejs,
+"opa": opa,
+"openshift": openshift,
+"oracle": oracle,
+"pagerduty": pagerduty,
+"php": php,
+"pod": pod,
+"postgres": postgres,
+"prometheus": prometheus,
+"python": python,
+"rabbitmq": rabbitmq,
+"rancher": rancher,
+"redhat": redhat,
+"redis": redis,
+"restic": restic,
+"ruby": ruby,
+"s3": s3,
+"s3Bucket": s3Bucket,
+"slack": slack,
+"smb": smb,
+"spring": spring,
+"sql": sql,
+"ssl": ssl,
+"stackdriver-monitoring": stackdriver_monitoring,
+"stackdriver-trace": stackdriver_trace,
+"stackdriver": stackdriver,
+"terraform": terraform,
+"trafficlight": trafficlight,
+"ubuntu": ubuntu,
+"vault": vault,
+"vsphere": vsphere,
+"www": www,
+}
