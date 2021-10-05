7efbad397237154be72f19144eececf8e03de8dd
John Goh <john@flanksource.com>
2021-10-05 15:47:39 +0800

    fix: revert to using /api as data source

--- src/pages/Examples/canary.js
+++ src/pages/Examples/canary.js
@@ -1,10 +1,9 @@
 import { Canary } from "../../components/Canary";
-import data from "../../data/sample.real.json";
 
 export function CanaryPage() {
   return (
     <div className="max-w-screen-xl mx-auto flex justify-center">
-      <Canary checks={data.checks} />
+      <Canary url="/api" />
     </div>
   );
 }
