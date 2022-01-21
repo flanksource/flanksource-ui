6be0d8428714e606208d8c501a5ba5c4d5a8472a
Oleksandr Sekker <oleksandr.sekker@onix-systems.com>
2022-01-21 14:57:40 +0200

    checkboxes

--- src/pages/Examples/TopologyPageSmall/TopologyPageSmall.js
+++ src/pages/Examples/TopologyPageSmall/TopologyPageSmall.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import topology from "../../../data/topology.json";
 import { TopologyPageSmallView } from "./TopologyPageSmallView";
 
-export const TopologyPageSmall = () => (
-  <TopologyPageSmallView topology={topology} />
-);
+export const TopologyPageSmall = () => {
+  const [selectionMode, setSelectionMode] = useState(false);
+  const [checked, setChecked] = useState({});
+  const toggleChecked = (id, checked) => {
+    setChecked((prevState) => ({ ...prevState, [id]: checked }));
+  };
+
+  return (
+    <TopologyPageSmallView
+      topology={topology}
+      checked={checked}
+      selectionMode={selectionMode}
+      setSelectionMode={setSelectionMode}
+      toggleChecked={toggleChecked}
+    />
+  );
+};
