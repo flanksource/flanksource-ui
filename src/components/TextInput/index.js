19ce3777e48694869fe7eec547a0d05644206632
Moshe Immerman <moshe@flanksource.com>
2022-02-05 21:41:06 +0200

    fix: log search on enter

--- src/components/TextInput/index.js
+++ src/components/TextInput/index.js
@@ -5,6 +5,7 @@ export function TextInput({
   id,
   className,
   labelClassName,
+  onEnter,
   type = "text",
   ...rest
 }) {
@@ -22,6 +23,12 @@ export function TextInput({
         type={type}
         name={id}
         id={id}
+        onKeyUp={(e) => {
+          if (onEnter != null && e.keyCode === 13) {
+            e.preventDefault();
+            onEnter();
+          }
+        }}
         className={`h-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 py-2 sm:text-sm border-gray-300 rounded-md ${className}`}
         {...rest}
       />
--- src/pages/logs-page.js
+++ src/pages/logs-page.js
@@ -1,19 +1,43 @@
 import { SearchIcon } from "@heroicons/react/solid";
-import { debounce } from "lodash";
-import React, { useContext, useEffect, useRef, useState } from "react";
-import { Controller, useForm } from "react-hook-form";
+import { isEmpty } from "lodash";
+import React, { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
 import { getLogs } from "../api/services/logs";
-import { Dropdown } from "../components/Dropdown";
-import { timeRanges } from "../components/Dropdown/TimeRange";
+import { getTopology } from "../api/services/topology";
+import { SearchLayout } from "../components/Layout";
+import { Loading } from "../components/Loading";
 import { LogsViewer } from "../components/Logs";
 import { TextInput } from "../components/TextInput";
-import { SearchLayout } from "../components/Layout"
 
 export function LogsPage() {
   const [logsIsLoading, setLogsIsLoading] = useState(true);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [query, setQuery] = useState(searchParams.get("query"));
+  const topologyId = searchParams.get("topologyId");
+  const [topology, setTopology] = useState(null);
+  useEffect(() => {
+    if (topologyId != null && topology == null) {
+      getTopology(topologyId).then((topology) => {
+        setTopology(topology.data[0]);
+      });
+    }
+  });
+
+  const saveQueryParams = () => {
+    const params = {};
+    if (!isEmpty(query)) {
+      params.query = query;
+    }
+    if (!isEmpty(topologyId)) {
+      params.topologyId = topologyId;
+    }
+    setSearchParams(params);
+  };
+
   const [logs, setLogs] = useState([]);
   const loadLogs = () => {
-    console.log("loading")
+    console.log("search", query);
+    saveQueryParams();
     setLogsIsLoading(true);
     getLogs().then((res) => {
       if (res.data != null) {
@@ -27,83 +51,72 @@ export function LogsPage() {
     loadLogs();
   }, []);
 
-  const [searchIsDirty, setSearchIsDirty] = useState(false);
-  const {
-    control,
-    watch,
-    formState: { isDirty }
-  } = useForm({
-    defaultValues: {
-      timeRange: timeRanges[0].value,
-      searchQuery: ""
-    }
-  });
-
-  const handleSearch = (searchQuery, timeRange) => {
-    console.log("search", searchQuery, timeRange);
-    // Call search API & update logs list here
-  };
-
-  const handleSearchDebounced = useRef(debounce(handleSearch, 700)).current;
-
-  const watchTimeRange = watch("timeRange");
-  const watchSearchQuery = watch("searchQuery");
-
-  useEffect(() => {
-    if (searchIsDirty) {
-      handleSearchDebounced(watchSearchQuery, watchTimeRange);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [watchSearchQuery, searchIsDirty]);
-
-  useEffect(() => {
-    if (searchIsDirty) {
-      handleSearch(watchSearchQuery, watchTimeRange);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [watchTimeRange, searchIsDirty]);
-
-  useEffect(() => {
-    setSearchIsDirty(true);
-  }, [isDirty]);
+  if (!isEmpty(topologyId) && topology == null) {
+    return <Loading text={`Loading topology ${topologyId}`} />;
+  }
 
   return (
     <SearchLayout
-      title={<div><h1 className="text-xl font-semibold">Logs</h1></div>}
+      title={
+        <div>
+          <h1 className="text-xl font-semibold">
+            Logs
+            {topology != null && (
+              <span className="text-gray-600">
+                / {topology.name || topology.text}
+              </span>
+            )}
+          </h1>
+        </div>
+      }
       onRefresh={loadLogs}
       extra={
         <>
           <div className="mr-4 w-72 relative rounded-md shadow-sm">
-            <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-              <SearchIcon
-                className="h-5 w-5 text-gray-400"
-                aria-hidden="true"
-              />
+            <div className="absolute inset-y-0 left-0 pl-3 flex items-center">
+              <button type="button" onClick={loadLogs} className="hover">
+                <SearchIcon
+                  className="h-5 w-5 text-gray-400 hover:text-gray-600"
+                  aria-hidden="true"
+                />
+              </button>
             </div>
-            <Controller
-              control={control}
-              name="searchQuery"
-              render={({ field }) => {
-                const { onChange, value } = field;
-                return (
-                  <TextInput
-                    placeholder="Search"
-                    className="pl-10 pb-2.5 w-full"
-                    style={{ height: "38px" }}
-                    id="searchQuery"
-                    onChange={onChange}
-                    value={value}
-                  />
-                );
+            <TextInput
+              placeholder="Search"
+              className="pl-10 pb-2.5 w-full"
+              style={{ height: "38px" }}
+              id="searchQuery"
+              onEnter={loadLogs}
+              onChange={(e) => {
+                e.preventDefault();
+                setQuery(e.target.value);
               }}
+              value={query}
             />
           </div>
-          <Dropdown
+          {/* <Dropdown
             control={control}
             name="timeRange"
             className="w-44"
             items={timeRanges}
           />
+          <Dropdown
+            control={control}
+            name="type"
+            className="w-44"
+            items={[
+              {
+                icon: <Icon name="kubernetes" />,
+                description: "Node",
+                value: "KubernetesNode"
+              },
+              {
+                icon: <Icon name="kubernetes" />,
+                description: "Pod",
+                value: "KubernetesDeployment"
+              }
+            ]}
+          /> */}
         </>
       }
     >
