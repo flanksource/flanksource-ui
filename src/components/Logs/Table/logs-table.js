f13ce8497c595a5f22d3c00fbd3c3ae1d0dcbba6
Oleksandr Sekker <oleksandr.sekker@onix-systems.com>
2022-02-07 10:04:20 +0200

    feat: logs in process

--- src/components/Logs/Table/logs-table.js
+++ src/components/Logs/Table/logs-table.js
@@ -1,7 +1,9 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import Convert from "ansi-to-html";
 import DOMPurify from "dompurify";
 import { isArray } from "lodash";
+import dayjs from "dayjs";
+import { useTable } from "react-table";
 
 export function LogsTable({ logs, actions = [] }) {
   if (logs != null && !isArray(logs)) {
@@ -24,6 +26,27 @@ export function LogsTable({ logs, actions = [] }) {
       setSelectedList([...selectedList].filter((obj) => log !== obj));
     }
   };
+  console.log(logs);
+  const columns = useMemo(
+    () => [
+      {
+        Header: "Name",
+        accessor: "timestamp"
+      },
+      {
+        Header: "Info",
+        accessor: "message"
+      }
+    ],
+    []
+  );
+  const data = useMemo(() => logs, [logs]);
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({
+      columns,
+      data
+    });
+  console.log(logs);
   return (
     <>
       <div className="mx-auto flex flex-col justify-center relative">
@@ -63,59 +86,95 @@ export function LogsTable({ logs, actions = [] }) {
         <div className="pb-12">
           <div className="flex flex-col">
             <div className="align-middle inline-block min-w-full">
-              <table
-                className="min-w-full divide-y divide-gray-300 border-separate"
-                style={{ borderSpacing: "0px" }}
-              >
-                <thead className="sticky top-0 bg-white">
-                  <tr>
-                    <th className="border-b border-gray-300" />
-                    <th className="text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-b border-gray-300 pr-5">
-                      Timestamp
-                    </th>
-                    <th className="text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-b border-gray-300">
-                      Message
-                    </th>
-                  </tr>
-                </thead>
-                <tbody
-                  className="bg-white divide-y divide-gray-300"
-                  style={{ fontSize: "0.75rem", lineHeight: "0.9rem" }}
-                >
-                  {logs.map((row, idx) => (
-                    // eslint-disable-next-line react/no-array-index-key
-                    <tr key={idx}>
-                      {actions.length > 0 && (
-                        <td className="border-b border-gray-100 ">
-                          <input
-                            type="checkbox"
-                            className="focus:ring-indigo-400 h-4 w-4 text-indigo-600 border-gray-300 rounded mr-2"
-                            onChange={(e) =>
-                              handleSelect(row, e.target.checked)
-                            }
-                            checked={selectedList.indexOf(row) >= 0}
-                          />
-                        </td>
-                      )}
-                      <td
-                        className="border-b border-gray-100 no-wrap py-1"
-                        style={{ whiteSpace: "nowrap" }}
-                      >
-                        {row.timestamp}
-                      </td>
-                      <td
-                        className="border-b border-gray-100"
-                        // eslint-disable-next-line react/no-danger
-                        dangerouslySetInnerHTML={{
-                          __html: DOMPurify.sanitize(
-                            convert.toHtml(row.message ? row.message : "")
-                          )
-                        }}
-                      />
+              <table {...getTableProps()}>
+                <thead>
+                  {headerGroups.map((headerGroup) => (
+                    <tr {...headerGroup.getHeaderGroupProps()}>
+                      {headerGroup.headers.map((column) => (
+                        <th {...column.getHeaderProps()}>
+                          {column.render("Header")}
+                        </th>
+                      ))}
                     </tr>
                   ))}
+                </thead>
+                <tbody {...getTableBodyProps()}>
+                  {rows.map((row, i) => {
+                    prepareRow(row);
+                    return (
+                      <tr {...row.getRowProps()}>
+                        {row.cells.map((cell) => (
+                          <td {...cell.getCellProps()}>
+                            {cell.render("Cell")}
+                          </td>
+                        ))}
+                      </tr>
+                    );
+                  })}
                 </tbody>
               </table>
+              {/* <table */}
+              {/*  className="min-w-full divide-y divide-gray-300 border-separate" */}
+              {/*  style={{ borderSpacing: "0px" }} */}
+              {/* > */}
+              {/*  <thead className="sticky top-0 bg-white"> */}
+              {/*    <tr> */}
+              {/*      <th className="text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-b border-gray-300 pr-5"> */}
+              {/*        Time */}
+              {/*      </th> */}
+              {/*      <th className="text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-b border-gray-300"> */}
+              {/*        Message */}
+              {/*      </th> */}
+              {/*    </tr> */}
+              {/*  </thead> */}
+              {/*  <tbody */}
+              {/*    className="bg-white divide-y divide-gray-300" */}
+              {/*    style={{ fontSize: "0.75rem", lineHeight: "0.9rem" }} */}
+              {/*  > */}
+              {/*    {logs.map((row, idx) => ( */}
+              {/*      // eslint-disable-next-line react/no-array-index-key */}
+              {/*      <tr key={idx}> */}
+              {/*        /!* {actions.length >= 0 && ( *!/ */}
+              {/*        /!*  <td className="border-b border-gray-100 "> *!/ */}
+              {/*        /!*    <input *!/ */}
+              {/*        /!*      type="checkbox" *!/ */}
+              {/*        /!*      className="focus:ring-indigo-400 h-4 w-4 text-indigo-600 border-gray-300 rounded mr-2" *!/ */}
+              {/*        /!*      onChange={(e) => *!/ */}
+              {/*        /!*        handleSelect(row, e.target.checked) *!/ */}
+              {/*        /!*      } *!/ */}
+              {/*        /!*      checked={selectedList.indexOf(row) >= 0} *!/ */}
+              {/*        /!*    /> *!/ */}
+              {/*        /!*  </td> *!/ */}
+              {/*        /!* )} *!/ */}
+              {/*        <td */}
+              {/*          className="border-b border-gray-100 no-wrap py-1 flex flex-row" */}
+              {/*          style={{ whiteSpace: "nowrap" }} */}
+              {/*        > */}
+              {/*          <input */}
+              {/*            type="checkbox" */}
+              {/*            className="focus:ring-indigo-400 h-4 w-4 text-indigo-600 border-gray-300 rounded mr-2" */}
+              {/*            onChange={(e) => handleSelect(row, e.target.checked)} */}
+              {/*            checked={selectedList.indexOf(row) >= 0} */}
+              {/*          /> */}
+              {/*          <p> */}
+              {/*            {dayjs(row.timestamp).format( */}
+              {/*              "MMM DD, YYYY HH:mm.ss.mmm" */}
+              {/*            )} */}
+              {/*          </p> */}
+              {/*        </td> */}
+              {/*        <td */}
+              {/*          className="border-b border-gray-100" */}
+              {/*          // eslint-disable-next-line react/no-danger */}
+              {/*          dangerouslySetInnerHTML={{ */}
+              {/*            __html: DOMPurify.sanitize( */}
+              {/*              convert.toHtml(row.message ? row.message : "") */}
+              {/*            ) */}
+              {/*          }} */}
+              {/*        /> */}
+              {/*      </tr> */}
+              {/*    ))} */}
+              {/*  </tbody> */}
+              {/* </table> */}
             </div>
           </div>
         </div>
