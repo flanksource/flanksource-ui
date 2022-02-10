07320bd6a8a8f89b1423bfd736d2e2e30e5d0fe0
Oleksandr Sekker <oleksandr.sekker@onix-systems.com>
2022-02-10 14:13:51 +0200

    refactor: improve

--- src/components/Logs/Table/logs-table.js
+++ src/components/Logs/Table/logs-table.js
@@ -10,7 +10,7 @@ import { IndeterminateCheckbox } from "../../IndeterminateCheckbox/Indeterminate
 
 const convert = new Convert();
 
-export function LogsTable({ logs, actions }) {
+export const LogsTable = ({ logs, actions }) => {
   if (logs != null && !isArray(logs)) {
     try {
       logs = JSON.parse(logs);
@@ -22,61 +22,52 @@ export function LogsTable({ logs, actions }) {
   if (logs == null) {
     logs = [];
   }
+  const timestampCell = ({ cell: { row } }) => (
+    <div className="min-w-max pl-6 pr-20 flex flex-row">
+      <div className="mr-1.5">
+        <IndeterminateCheckbox
+          className="focus:ring-indigo-400 h-4 w-4 text-indigo-600 border-gray-300 rounded"
+          {...row.getToggleRowSelectedProps()}
+        />
+      </div>
+      <p>{dayjs(row.original.timestamp).format("MMM DD, YYYY HH:mm.ss.SSS")}</p>
+    </div>
+  );
+  const messageCell = ({ cell: { value } }) => (
+    <div
+      className="pl-6 pr-12"
+      // eslint-disable-next-line react/no-danger
+      dangerouslySetInnerHTML={{
+        __html: DOMPurify.sanitize(convert.toHtml(value || ""))
+      }}
+    />
+  );
+  const buttonHeader = () => (
+    <button
+      className="bg-dark-blue text-white text-sm leading-4 font-medium py-2 my-1 mr-2 rounded-6px w-44"
+      type="button"
+      onClick={() => {}}
+    >
+      Create new hypothesis
+    </button>
+  );
+
   const columns = useMemo(
     () => [
       {
         id: "selection",
         Header: "Time",
         accessor: "timestamp",
-        Cell: function dataItems({
-          cell: {
-            row,
-            row: {
-              original: { timestamp }
-            }
-          }
-        }) {
-          return (
-            <div className="min-w-max pl-6 pr-20 flex flex-row">
-              <div className="mr-1.5">
-                <IndeterminateCheckbox
-                  className="focus:ring-indigo-400 h-4 w-4 text-indigo-600 border-gray-300 rounded"
-                  {...row.getToggleRowSelectedProps()}
-                />
-              </div>
-              <p>{dayjs(timestamp).format("MMM DD, YYYY HH:mm.ss.SSS")}</p>
-            </div>
-          );
-        }
+        Cell: timestampCell
       },
       {
         Header: "Message",
         accessor: "message",
         id: "message",
-        Cell: function dataMessage({ cell: { value } }) {
-          return (
-            <div
-              className="pl-6 pr-12"
-              // eslint-disable-next-line react/no-danger
-              dangerouslySetInnerHTML={{
-                __html: DOMPurify.sanitize(convert.toHtml(value || ""))
-              }}
-            />
-          );
-        }
+        Cell: messageCell
       },
       {
-        Header: function butonHeader() {
-          return (
-            <button
-              className="bg-dark-blue text-white text-sm leading-4 font-medium py-2 my-1 mr-2 rounded-6px w-44"
-              type="button"
-              onClick={() => {}}
-            >
-              Create new hypothesis
-            </button>
-          );
-        },
+        Header: buttonHeader,
         id: "button"
       }
     ],
@@ -102,101 +93,96 @@ export function LogsTable({ logs, actions }) {
     }
   );
   return (
-    <>
-      <div className="mx-auto flex flex-col justify-center relative">
-        {actions.length >= 0 && (
-          <div
-            className="flex justify-between items-center overflow-y-hidden mb-4"
-            style={{
-              maxHeight: Object.keys(selectedRowIds).length ? "100px" : "0px",
-              transition: "max-height 0.25s  ease-in-out"
-            }}
-          >
-            <div>
-              {Object.keys(selectedRowIds).length > 0 && (
-                <span className="text-sm text-gray-400">
-                  {Object.keys(selectedRowIds).length} selected
-                </span>
-              )}
-            </div>
-
-            {actions.map((action) => (
-              <button
-                key={action.label}
-                type="button"
-                disabled={!Object.keys(selectedRowIds).length}
-                onClick={() => {
-                  action.handler(selectedFlatRows.map((d) => d.original));
-                }}
-                className={clsx(
-                  Object.keys(selectedRowIds).length > 0
-                    ? "text-indigo-700 bg-indigo-100 hover:bg-indigo-200"
-                    : "text-gray-400 bg-gray-200",
-                  "inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded"
-                )}
-              >
-                {action.label}
-              </button>
-            ))}
+    <div className="mx-auto flex flex-col justify-center relative">
+      {actions.length >= 0 && (
+        <div
+          className="flex justify-between items-center overflow-y-hidden mb-4"
+          style={{
+            maxHeight: Object.keys(selectedRowIds).length ? "100px" : "0px",
+            transition: "max-height 0.25s  ease-in-out"
+          }}
+        >
+          <div>
+            {Object.keys(selectedRowIds).length > 0 && (
+              <span className="text-sm text-gray-400">
+                {Object.keys(selectedRowIds).length} selected
+              </span>
+            )}
           </div>
-        )}
 
-        <div className="pb-12">
-          <div className="flex flex-col">
-            <div className="inline-block min-w-full">
-              <table
-                className="w-full border rounded-t-6px"
-                {...getTableProps()}
-              >
-                <thead className="bg-lightest-gray border rounded-t-6px">
-                  {headerGroups.map((headerGroup) => {
-                    const { key, ...restHeaderGroupProps } =
-                      headerGroup.getHeaderGroupProps();
-                    return (
-                      <tr key={key} {...restHeaderGroupProps}>
-                        {headerGroup.headers.map((column) => (
-                          <th
-                            key={column.id}
-                            className="pl-6 text-medium-gray text-xs leading-4 font-medium tracking-wider uppercase text-left"
-                            {...column.getHeaderProps()}
-                          >
-                            {column.render("Header")}
-                          </th>
-                        ))}
-                      </tr>
-                    );
-                  })}
-                </thead>
-                <tbody {...getTableBodyProps()}>
-                  {rows.map((row) => {
-                    prepareRow(row);
-                    return (
-                      <tr
-                        className="border-b"
-                        key={row.id}
-                        {...row.getRowProps()}
-                      >
-                        {row.cells.map((cell) => (
-                          <td
-                            key={cell.row.id}
-                            className=" py-4 text-darker-black text-sm"
-                            {...cell.getCellProps()}
-                          >
-                            {cell.render("Cell")}
-                          </td>
-                        ))}
-                      </tr>
-                    );
-                  })}
-                </tbody>
-              </table>
-            </div>
+          {actions.map((action) => (
+            <button
+              key={action.label}
+              type="button"
+              disabled={!Object.keys(selectedRowIds).length}
+              onClick={() => {
+                action.handler(selectedFlatRows.map((d) => d.original));
+              }}
+              className={clsx(
+                Object.keys(selectedRowIds).length > 0
+                  ? "text-indigo-700 bg-indigo-100 hover:bg-indigo-200"
+                  : "text-gray-400 bg-gray-200",
+                "inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded"
+              )}
+            >
+              {action.label}
+            </button>
+          ))}
+        </div>
+      )}
+
+      <div className="pb-12">
+        <div className="flex flex-col">
+          <div className="inline-block min-w-full">
+            <table className="w-full border rounded-t-6px" {...getTableProps()}>
+              <thead className="bg-lightest-gray border rounded-t-6px">
+                {headerGroups.map((headerGroup) => {
+                  const { key, ...restHeaderGroupProps } =
+                    headerGroup.getHeaderGroupProps();
+                  return (
+                    <tr key={key} {...restHeaderGroupProps}>
+                      {headerGroup.headers.map((column) => (
+                        <th
+                          key={column.id}
+                          className="pl-6 text-medium-gray text-xs leading-4 font-medium tracking-wider uppercase text-left"
+                          {...column.getHeaderProps()}
+                        >
+                          {column.render("Header")}
+                        </th>
+                      ))}
+                    </tr>
+                  );
+                })}
+              </thead>
+              <tbody {...getTableBodyProps()}>
+                {rows.map((row) => {
+                  prepareRow(row);
+                  return (
+                    <tr
+                      className="border-b"
+                      key={row.id}
+                      {...row.getRowProps()}
+                    >
+                      {row.cells.map((cell) => (
+                        <td
+                          key={cell.row.id}
+                          className=" py-4 text-darker-black text-sm"
+                          {...cell.getCellProps()}
+                        >
+                          {cell.render("Cell")}
+                        </td>
+                      ))}
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
-}
+};
 LogsTable.propTypes = {
   logs: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
   actions: PropTypes.arrayOf(PropTypes.shape({}))
