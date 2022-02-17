cfc80e638f82b159066a2aebf2fadea1b1a332c6
Oleksandr Sekker <oleksandr.sekker@onix-systems.com>
2022-02-17 15:48:10 +0200

    fix: make fixes in create hypothesis

--- src/components/HypothesisBuilder/components/CreateHypothesis/index.js
+++ src/components/HypothesisBuilder/components/CreateHypothesis/index.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { useForm } from "react-hook-form";
+import React from "react";
+import { useForm, Controller } from "react-hook-form";
 import { v4 as uuidv4 } from "uuid";
 import { Dropdown } from "../../../Dropdown";
 import { hypothesisStatuses } from "../../data";
@@ -8,6 +8,30 @@ import { EvidenceSection } from "../EvidenceSection";
 import { useUser } from "../../../../context";
 import { createComment } from "../../../../api/services/comments";
 import { toastError } from "../../../Toast/toast";
+import { HypothesisStatuses } from "../../../../constants/hypothesis-statuses";
+
+const statusItems = {
+  ...Object.values(hypothesisStatuses).reduce((acc, obj) => {
+    const title = obj.title.toLowerCase();
+    acc[title] = {
+      id: `dropdown-${title}`,
+      name: title,
+      icon: React.createElement(obj.icon.type, {
+        color: obj.color,
+        style: { width: "20px" }
+      }),
+      description: obj.title,
+      value: title
+    };
+    return acc;
+  }, {})
+};
+
+const nodePathLengthToNewNodeTypeMapping = {
+  1: "root",
+  2: "factor",
+  3: "solution"
+};
 
 export const CreateHypothesis = ({
   nodePath,
@@ -15,55 +39,46 @@ export const CreateHypothesis = ({
   api,
   onHypothesisCreated
 }) => {
-  const [evidenceChecked, setEvidenceChecked] = useState([]);
   const user = useUser();
-  const node = getNode(tree, nodePath);
-  const statusItems = {
-    ...Object.values(hypothesisStatuses).reduce((acc, obj) => {
-      const title = obj.title.toLowerCase();
-      acc[title] = {
-        id: `dropdown-${title}`,
-        name: title,
-        icon: React.createElement(obj.icon.type, {
-          color: obj.color,
-          style: { width: "20px" }
-        }),
-        description: obj.title,
-        value: title
-      };
-      return acc;
-    }, {})
-  };
-  const { control, watch, getValues, setValue, handleSubmit } = useForm({
+  const parentNode = getNode(tree, nodePath);
+  const { control, getValues, setValue, handleSubmit, watch } = useForm({
     defaultValues: {
-      status: node.status || Object.values(statusItems)[2].value,
-      title: "",
-      comments: ""
+      hypothesis: {
+        status: HypothesisStatuses.Possible,
+        title: ""
+      },
+      evidence: {},
+      comment: {
+        text: ""
+      }
     }
   });
-  const nameValue = watch("title");
-  const commentsValue = watch("comments");
-  const watchStatus = watch("status");
-  const handleComment = (value) =>
-    createComment(user, uuidv4(), node.incident_id, node.id, value)
+  const evidenceValue = watch("evidence");
+  const handleComment = (nodeId, value) =>
+    createComment(user, uuidv4(), parentNode.incident_id, nodeId, value)
       .then((response) => console.log(response))
       .catch(toastError);
 
   const onSubmit = async () => {
     const newNodeID = uuidv4();
     if (api?.createMutation) {
-      await api.createMutation.mutateAsync({
-        user,
-        id: newNodeID,
-        incidentId: api.incidentId,
-        params: {
-          title: getValues("title"),
-          type: node.type,
-          status: watchStatus
-        }
-      });
+      try {
+        const newNodeResponse = await api.createMutation.mutateAsync({
+          user,
+          id: newNodeID,
+          incidentId: api.incidentId,
+          params: {
+            title: getValues("hypothesis.title"),
+            type: nodePathLengthToNewNodeTypeMapping[nodePath.length + 1],
+            status: getValues("hypothesis.status")
+          }
+        });
+        const newNode = newNodeResponse.data[0];
+        await handleComment(newNode.id, getValues("comment.text"));
+      } catch (e) {
+        toastError(e);
+      }
     }
-    await handleComment(getValues("comments"));
     onHypothesisCreated();
   };
   return (
@@ -73,48 +88,55 @@ export const CreateHypothesis = ({
         <div className="text-sm font-medium text-gray-700 mb-1.5">
           <label htmlFor="name">Name</label>
         </div>
-        <input
-          id="name"
-          type="text"
-          placeholder="Type name"
-          onChange={(e) => {
-            setValue("title", e.target.value);
-          }}
-          value={nameValue}
-          className="w-72 cursor-pointer bg-white border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm text-gray-500 text-base leading-6 font-normal"
+        <Controller
+          name="hypothesis.title"
+          control={control}
+          render={({ field }) => (
+            <input
+              className="w-72 cursor-pointer bg-white border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm text-gray-500 text-base leading-6 font-normal"
+              type="text"
+              placeholder="Type name"
+              {...field}
+            />
+          )}
         />
       </div>
       <div className="mt-4">
         <Dropdown
           control={control}
-          name="status"
+          name="hypothesis.status"
           className="mb-4 w-72"
           items={statusItems}
           label="State"
         />
       </div>
       <EvidenceSection
-        hypothesis={node}
-        evidence={evidenceChecked}
+        hypothesis={parentNode}
+        evidence={evidenceValue}
         titlePrepend={
           <div className="mr-2 text-sm font-medium text-gray-700 mt-1.5">
             Evidence
           </div>
         }
         onButtonClick={(data) => {
-          setEvidenceChecked(data);
+          setValue("evidence", data);
         }}
       />
       <div>
         <h3 className="mt-4 text-sm font-medium text-gray-700 mb-1.5">
           Comment
         </h3>
-        <textarea
-          className="w-full border-gray-200 resize-none rounded-6px text-base leading-6 font-normal font-inter outline-none placeholder-gray-400"
-          onChange={(e) => setValue("comments", e.target.value)}
-          placeholder="Type something"
-          value={commentsValue}
-          style={{ minHeight: "138px" }}
+        <Controller
+          name="comment.text"
+          control={control}
+          render={({ field }) => (
+            <textarea
+              className="w-full border-gray-200 resize-none rounded-6px text-base leading-6 font-normal font-inter outline-none placeholder-gray-400"
+              placeholder="Type something"
+              style={{ minHeight: "138px" }}
+              {...field}
+            />
+          )}
         />
       </div>
       <div className="flex justify-end mt-4">
