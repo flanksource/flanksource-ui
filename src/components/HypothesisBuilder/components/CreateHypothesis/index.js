adafe8ca8e8eb6f7ac22607bc57ac26cfa3e0324
Oleksandr Sekker <oleksandr.sekker@onix-systems.com>
2022-02-17 12:03:50 +0200

    feat: makes updates and fixes

--- src/components/HypothesisBuilder/components/CreateHypothesis/index.js
+++ src/components/HypothesisBuilder/components/CreateHypothesis/index.js
@@ -1,22 +1,21 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
+import { v4 as uuidv4 } from "uuid";
 import { Dropdown } from "../../../Dropdown";
 import { hypothesisStatuses } from "../../data";
 import { getNode } from "../../../NestedHeirarchy/utils";
 import { EvidenceSection } from "../EvidenceSection";
 import { useUser } from "../../../../context";
+import { createComment } from "../../../../api/services/comments";
+import { toastError } from "../../../Toast/toast";
 
 export const CreateHypothesis = ({
   nodePath,
-  treeFunctions,
   tree,
   api,
   onHypothesisCreated
 }) => {
-  const [commentTextValue, setCommentTextValue] = useState("");
-  const [nameValue, setNameValue] = useState("");
   const [evidenceChecked, setEvidenceChecked] = useState([]);
-  const { handleAddNode } = treeFunctions;
   const user = useUser();
   const node = getNode(tree, nodePath);
   const statusItems = {
@@ -35,31 +34,40 @@ export const CreateHypothesis = ({
       return acc;
     }, {})
   };
-  const { control, watch } = useForm({
+  const { control, watch, getValues, setValue, handleSubmit } = useForm({
     defaultValues: {
-      status: node.status || Object.values(statusItems)[2].value
+      status: node.status || Object.values(statusItems)[2].value,
+      title: "",
+      comments: ""
     }
   });
-
+  const nameValue = watch("title");
+  const commentsValue = watch("comments");
   const watchStatus = watch("status");
-  const onSubmit = () => {
-    const newNodeID = handleAddNode(nodePath);
+  const handleComment = (value) =>
+    createComment(user, uuidv4(), node.incident_id, node.id, value)
+      .then((response) => console.log(response))
+      .catch(toastError);
+
+  const onSubmit = async () => {
+    const newNodeID = uuidv4();
     if (api?.createMutation) {
-      api.createMutation.mutate({
+      await api.createMutation.mutateAsync({
         user,
         id: newNodeID,
         incidentId: api.incidentId,
         params: {
-          title: nameValue,
+          title: getValues("title"),
           type: node.type,
           status: watchStatus
         }
       });
     }
+    await handleComment(getValues("comments"));
     onHypothesisCreated();
   };
   return (
-    <div>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <h2 className="text-2xl font-semibold text-gray-700">Creat hypothesis</h2>
       <div className="mt-6">
         <div className="text-sm font-medium text-gray-700 mb-1.5">
@@ -69,7 +77,9 @@ export const CreateHypothesis = ({
           id="name"
           type="text"
           placeholder="Type name"
-          onChange={(e) => setNameValue(e.target.value)}
+          onChange={(e) => {
+            setValue("title", e.target.value);
+          }}
           value={nameValue}
           className="w-72 cursor-pointer bg-white border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm text-gray-500 text-base leading-6 font-normal"
         />
@@ -87,9 +97,9 @@ export const CreateHypothesis = ({
         hypothesis={node}
         evidence={evidenceChecked}
         titlePrepend={
-          <h3 className="mr-2 text-sm font-medium text-gray-700 mt-1.5">
+          <div className="mr-2 text-sm font-medium text-gray-700 mt-1.5">
             Evidence
-          </h3>
+          </div>
         }
         onButtonClick={(data) => {
           setEvidenceChecked(data);
@@ -101,22 +111,20 @@ export const CreateHypothesis = ({
         </h3>
         <textarea
           className="w-full border-gray-200 resize-none rounded-6px text-base leading-6 font-normal font-inter outline-none placeholder-gray-400"
-          onChange={(e) => setCommentTextValue(e.target.value)}
+          onChange={(e) => setValue("comments", e.target.value)}
           placeholder="Type something"
-          value={commentTextValue}
+          value={commentsValue}
           style={{ minHeight: "138px" }}
         />
       </div>
       <div className="flex justify-end mt-4">
-        <button
-          type="button"
+        <input
+          type="submit"
           className="bg-dark-blue text-white rounded-6px py-3.5 px-6 disabled:bg-gray-200 disabled:text-gray-400 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
           disabled={false}
-          onClick={onSubmit}
-        >
-          Create
-        </button>
+          value="Create"
+        />
       </div>
-    </div>
+    </form>
   );
 };
