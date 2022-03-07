f7da638cd898851c0392254f07e1153a6be8ae37
Dmitrii Melnik <dmitrii.melnik@onix-systems.com>
2022-03-07 21:18:03 +0200

    fix: modal improvements

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,68 +1,23 @@
+import React, { Fragment } from "react";
+import PropTypes from "prop-types";
 import { Dialog, Transition } from "@headlessui/react";
 import { XIcon } from "@heroicons/react/solid";
-import React, { Fragment } from "react";
-
-const sizeModal = {
-  small: "small",
-  medium: "medium",
-  full: "full"
-};
-
-const listClasses = {
-  small: {
-    cardClass: "w-full overflow-y-auto",
-    contentClass: "h-full px-8",
-    cardStyle: {
-      maxWidth: "420px",
-      maxHeight: "calc(100vh - 4rem)",
-      margin: "0 10px"
-    },
-    closeButtonClass: "w-6 h-6"
-  },
-  medium: {
-    cardClass: "w-full overflow-y-auto",
-    contentClass: "h-full px-8",
-    cardStyle: {
-      maxWidth: "1024px",
-      maxHeight: "calc(100vh - 4rem)",
-      margin: "0 15px"
-    },
-    closeButtonClass: "w-6 h-6"
-  },
-  full: {
-    cardClass: "w-full overflow-y-auto",
-    contentClass: "h-full px-8",
-    cardStyle: {
-      maxWidth: "1280px",
-      maxHeight: "calc(100vh - 4rem)",
-      margin: "0 20px"
-    },
-    closeButtonClass: "w-6 h-6",
-    containerClass: "py-8"
-  }
-};
+import clsx from "clsx";
+import "./index.css";
 
 export function Modal({
   title,
   body,
   open,
   onClose,
-  allowBackgroundClose = true,
+  allowBackgroundClose,
   hideCloseButton,
-  cancelText = "Close",
+  cancelText,
   hideActions,
-  size = sizeModal.medium,
+  size,
   ...rest
 }) {
   const { children } = { ...rest };
-  const {
-    cardClass,
-    contentClass,
-    cardStyle,
-    closeButtonClass,
-    closeButtonStyle = {},
-    containerClass = ""
-  } = listClasses[size];
   return (
     <Transition.Root show={open} as={Fragment}>
       <Dialog
@@ -73,7 +28,9 @@ export function Modal({
         {...rest}
       >
         <div
-          className={`flex items-center justify-center h-full ${containerClass}`}
+          className={clsx("flex items-center justify-center h-full", {
+            "py-8": size === "full"
+          })}
         >
           <Transition.Child
             as={Fragment}
@@ -97,50 +54,42 @@ export function Modal({
             leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           >
             <div
-              style={cardStyle}
-              className={`bg-white rounded-lg text-left shadow-xl transform transition-all ${cardClass}`}
+              className={clsx(
+                "bg-white rounded-lg text-left shadow-xl transform transition-all w-full overflow-y-auto",
+                `modal-card-${size}`
+              )}
             >
               <div className="mt-8 px-8 flex justify-between items-center ">
                 <h1 className="font-semibold text-lg">{title}</h1>
                 {/* top-right close button */}
                 {!hideCloseButton && (
-                  <div
-                    className="pointer-events-none sm:pointer-events-auto"
-                    style={closeButtonStyle}
-                  >
+                  <div className="pointer-events-none sm:pointer-events-auto">
                     <button
                       type="button"
                       className="bg-white rounded-md text-gray-400 hover:text-gray-500 focus:outline-none"
                       onClick={onClose}
                     >
                       <span className="sr-only">Close</span>
-                      <XIcon
-                        className={`${closeButtonClass}`}
-                        aria-hidden="true"
-                      />
+                      <XIcon className="w-6 h-6" aria-hidden="true" />
                     </button>
                   </div>
                 )}
               </div>
-              <div
-                className={contentClass}
-                style={{ maxHeight: "80vh", overflowY: "auto" }}
-              >
+              <div className="h-full px-8 modal-card__container">
                 {children}
               </div>
-              <div style={{ minHeight: "1vh" }}>
-                {!hideActions && (
-                  <div className="flex justify-end mt-2">
-                    <button
-                      type="button"
-                      className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:w-auto sm:text-sm"
-                      onClick={onClose}
-                    >
-                      {cancelText}
-                    </button>
-                  </div>
-                )}
-              </div>
+
+              {!hideActions && (
+                <div className="flex justify-end mt-2">
+                  <button
+                    type="button"
+                    className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:w-auto sm:text-sm"
+                    onClick={onClose}
+                  >
+                    {cancelText}
+                  </button>
+                </div>
+              )}
             </div>
           </Transition.Child>
         </div>
@@ -148,3 +97,23 @@ export function Modal({
     </Transition.Root>
   );
 }
+
+Modal.propTypes = {
+  title: PropTypes.string,
+  open: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+  allowBackgroundClose: PropTypes.bool,
+  hideCloseButton: PropTypes.bool,
+  cancelText: PropTypes.string,
+  hideActions: PropTypes.bool,
+  size: PropTypes.oneOf(["small", "medium", "full"])
+};
+
+Modal.defaultProps = {
+  title: "",
+  allowBackgroundClose: true,
+  hideCloseButton: false,
+  cancelText: "Close",
+  hideActions: false,
+  size: "medium"
+};
