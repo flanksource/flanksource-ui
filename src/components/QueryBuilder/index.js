323ca915d73808431e60f4955a2f72858cedbc68
John Goh <john@flanksource.com>
2022-04-21 15:37:02 +0800

    feat: API integration for Query Builder on Config page

--- src/components/QueryBuilder/index.js
+++ src/components/QueryBuilder/index.js
@@ -1,7 +1,16 @@
 import React, { useEffect, useState } from "react";
-import { getAllSavedQueries } from "../../api/services/configs";
+import history from "history/browser";
+import { TrashIcon } from "@heroicons/react/solid";
+import toast from "react-hot-toast";
+import {
+  createSavedQuery,
+  deleteSavedQuery,
+  getAllSavedQueries
+} from "../../api/services/configs";
+import { decodeUrlSearchParams, updateParams } from "../Canary/url";
 import { Dropdown } from "../Dropdown";
 import { Modal } from "../Modal";
+import { toastError } from "../Toast/toast";
 
 //   const columnSelectDropdownStyles = {
 //     valueContainer: (provided) => ({
@@ -21,16 +30,74 @@ import { Modal } from "../Modal";
 //   };
 
 export const QueryBuilder = () => {
+  const [searchParams, setSearchParams] = useState(
+    decodeUrlSearchParams(window.location.search)
+  );
+  useEffect(() => {
+    history.listen(({ location }) => {
+      setSearchParams(decodeUrlSearchParams(location.search));
+    });
+  }, []);
+  const { query } = searchParams;
+
   //   const [selectedColumns, setSelectedColumns] = useState([]);
   const [selectedQuery, setSelectedQuery] = useState(null);
   const [queryList, setQueryList] = useState([]);
   const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [presetTextValue, setPresetTextValue] = useState("");
+  const [savePresetIsLoading, setSavePresetIsLoading] = useState(false);
 
-  useEffect(() => {
+  const fetchQueries = () => {
     getAllSavedQueries().then((res) => {
       setQueryList(res.data);
+      setSelectedQuery(null);
     });
+  };
+
+  useEffect(() => {
+    fetchQueries();
   }, []);
+
+  useEffect(() => {
+    if (selectedQuery) {
+      const queryObj = queryList.find((o) => o.id === selectedQuery);
+      if (queryObj) {
+        const { query } = queryObj;
+        updateParams({ query });
+      }
+    } else {
+      updateParams({ query: "" });
+    }
+  }, [selectedQuery, queryList]);
+
+  const handleSearch = (value) => {
+    updateParams({ query: value });
+  };
+
+  const handleQuerySave = () => {
+    setSavePresetIsLoading(true);
+    createSavedQuery(query, { description: presetTextValue })
+      .then((res) => {
+        toast(res.statusText);
+        fetchQueries();
+      })
+      .catch((err) => {
+        toastError(err);
+      })
+      .finally(() => {
+        setModalIsOpen(false);
+        setSavePresetIsLoading(false);
+      });
+  };
+  const handleQueryDelete = (presetID) => {
+    deleteSavedQuery(presetID).then((res) => {
+      const { status } = res;
+      if (status === 200 || status === 201) {
+        toast("Preset deleted");
+        fetchQueries();
+      }
+    });
+  };
 
   const exampleQueries = queryList.reduce(
     (acc, current) => {
@@ -72,6 +139,15 @@ export const QueryBuilder = () => {
             </>
           }
         />
+        {selectedQuery && (
+          <button
+            className="border border-gray-300 rounded-md px-1.5 py-1.5 text-sm  whitespace-nowrap text-white bg-red-500 mr-2"
+            type="button"
+            onClick={() => handleQueryDelete(selectedQuery)}
+          >
+            <TrashIcon height={18} width={18} />
+          </button>
+        )}
         <button
           className="border border-gray-300 rounded-md px-3 py-2 text-sm bg-white whitespace-nowrap text-gray-800"
           type="button"
@@ -84,6 +160,8 @@ export const QueryBuilder = () => {
       <div className="flex flex-wrap">
         <input
           type="text"
+          value={query}
+          onChange={(e) => handleSearch(e.target.value)}
           className="w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-400 block py-2 sm:text-sm border-gray-300 rounded-md mb-2"
           style={{ minWidth: "300px" }}
           placeholder="Query text (name, namespace, type, or description)"
@@ -106,15 +184,22 @@ export const QueryBuilder = () => {
         <div className="flex flex-col pt-5 pb-3 max-w-lg" style={{}}>
           <input
             type="text"
+            defaultValue={presetTextValue}
             className="w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-400 block py-2 sm:text-sm border-gray-300 rounded-md mr-2 mb-2"
             style={{ minWidth: "300px" }}
             placeholder="Preset name"
+            onChange={(e) => setPresetTextValue(e.target.value)}
           />
           <div className="flex justify-end mt-3">
             <button
-              className="border border-gray-300 rounded-md px-3 py-2 text-sm bg-white whitespace-nowrap text-gray-800"
+              disabled={presetTextValue.length <= 0 || savePresetIsLoading}
+              className={`border border-gray-300 rounded-md px-3 py-2 text-sm whitespace-nowrap ${
+                presetTextValue.length <= 0 || savePresetIsLoading
+                  ? "bg-gray-200  text-gray-400 cursor-not-allowed"
+                  : "bg-indigo-700 text-gray-50 cursor-pointer"
+              }`}
               type="button"
-              onClick={() => setModalIsOpen(true)}
+              onClick={() => handleQuerySave()}
             >
               Save as preset
             </button>
