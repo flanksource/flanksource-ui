0a9a05cb8c06081a734b9af159ce6958d49eefa3
John Goh <john@flanksource.com>
2022-04-26 15:31:18 +0800

    fix: tailwind css styles absent on `npm run build-storybook`

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,11 +1,13 @@
-import '../src/index.css';
+import "!style-loader!css-loader!postcss-loader!tailwindcss/tailwind.css";
+import "tailwindcss/tailwind.css";
+import "../src/index.css";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
     matchers: {
       color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-}
+      date: /Date$/
+    }
+  }
+};
