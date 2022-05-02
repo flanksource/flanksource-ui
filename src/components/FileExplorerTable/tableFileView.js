5d253f12baae1d13ec299b5ecaff676d4e141ec6
Jordan Atanasov <jatanasov@jatanasov-a02.vmware.com>
2022-05-02 17:06:28 +0300

    pre-paging state

--- src/components/FileExplorerTable/tableFileView.js
+++ src/components/FileExplorerTable/tableFileView.js
@@ -5,7 +5,7 @@ import { useTable, useFilters } from "react-table";
 
 
 //Simple recursive TreeView Component
-export const TableFileView = ({ columns, data, stateChanger}) => {
+export const TableFileView = ({ columns, data, stateChanger }) => {
 
     //use the state to keep track of open/closed folders
     const [isExpanded, toggleExpanded] = useState(false);
@@ -71,6 +71,50 @@ export const TableFileView = ({ columns, data, stateChanger}) => {
                     })}
                 </tbody>
             </table>
+            <div className="pagination">
+                <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+                    {'<<'}
+                </button>{' '}
+                <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+                    {'<'}
+                </button>{' '}
+                <button onClick={() => nextPage()} disabled={!canNextPage}>
+                    {'>'}
+                </button>{' '}
+                <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+                    {'>>'}
+                </button>{' '}
+                <span>
+                    Page{' '}
+                    <strong>
+                        {pageIndex + 1} of {pageOptions.length}
+                    </strong>{' '}
+                </span>
+                <span>
+                    | Go to page:{' '}
+                    <input
+                        type="number"
+                        defaultValue={pageIndex + 1}
+                        onChange={e => {
+                            const page = e.target.value ? Number(e.target.value) - 1 : 0
+                            gotoPage(page)
+                        }}
+                        style={{ width: '100px' }}
+                    />
+                </span>{' '}
+                <select
+                    value={pageSize}
+                    onChange={e => {
+                        setPageSize(Number(e.target.value))
+                    }}
+                >
+                    {[10, 20, 30, 40, 50].map(pageSize => (
+                        <option key={pageSize} value={pageSize}>
+                            Show {pageSize}
+                        </option>
+                    ))}
+                </select>
+            </div>
         </>
     );
 
--- src/pages/file-explorer-page.js
+++ src/pages/file-explorer-page.js
@@ -3,6 +3,9 @@ import { TreeView } from "../components/FileExplorer";
 import files from "../components/FileExplorerTable/files.json"
 import { TableFileView } from "../components/FileExplorerTable";
 import { Icon } from "../components/Icon";
+import DocViewer from "react-doc-viewer";
+import Moment from 'moment';
+//import fs from 'fs';
 
 // A third party file-viewer component to render the selected files, purely for demo purposes
 import { FileViewer } from "react-file-viewer-v2";
@@ -14,13 +17,43 @@ export const FileExplorerPage = () => {
   const [selectedFile, setSelectedFile] = useState({});
 
   const changeFilePath = (file) => {
-      setSelectedFile(file);
-      console.log(file);
+    fetch(file)
+      .then(response => response.text())
+      .then(data => {
+        // Do something with your data
+        console.log(data);
+        setSelectedFile(data);
+      });
   }
 
-  const onError = e => {
-    console.log(e, "error in file-viewer");
-  };
+
+
+  function formatSize(numberString) {
+    let number = parseFloat(numberString);
+    if (numberString > 1024) {
+      numberString = numberString / 1024;
+      return numberString + "MB";
+    }
+    else {
+      return "0." + numberString + "MB";
+    }
+  }
+
+  function formatDate(dateString) {
+    try {
+      const year = +dateString.substring(0, 4);
+      const month = +dateString.substring(6, 7);
+      const day = +dateString.substring(7, 9);
+
+      console.log(new Date(year, month - 1, day).toDateString());
+
+      return new Date(year, month - 1, day).toDateString();
+    }
+    catch {
+      return dateString
+    }
+
+  }
 
   // Custom component to render Genres 
   const IconCell = ({ icon }) => {
@@ -62,15 +95,18 @@ export const FileExplorerPage = () => {
         columns: [
           {
             Header: "Size",
-            accessor: "size"
+            accessor: "size",
+            Cell: ({ cell: { value } }) => <div> {formatSize(value)} </div>
           },
           {
             Header: "Created",
-            accessor: "createdDate"
+            accessor: "createdDate",
+            Cell: ({ cell: { value } }) => <div> {formatDate(value)} </div>
           },
           {
             Header: "Last Modified",
-            accessor: "lastModified"
+            accessor: "lastModified",
+            Cell: ({ cell: { value } }) => <div> {formatDate(value)} </div>
           }
         ]
       }
@@ -82,6 +118,9 @@ export const FileExplorerPage = () => {
     <>
       <div className="file-explorer-container">
         <TableFileView columns={columns} data={files} stateChanger={changeFilePath} />
+      </div>
+      <div className="file-viewer-container">
+        <textarea value={selectedFile} onChange={changeFilePath} className="file-display" multiline={true} />
       </div>
     </>
   );
