e608e7f5557916b509670e48cc542c77bcb8ca05
Prashanth-Nelli <micprashanth@gmail.com>
2022-05-03 21:03:13 +0530

    component memoization changes

--- src/components/BannerMessage/index.js
+++ src/components/BannerMessage/index.js
@@ -1,10 +1,12 @@
-export function BannerMessage({ title, subtitle, prepend, append }) {
-  return (
-    <div className="flex flex-col p-16 items-center justify-center text-center">
-      <div>{prepend}</div>
-      <div className="text-4xl text-gray-800 font-bold mb-2">{title}</div>
-      <div className="text-md text-gray-400 mb-4">{subtitle}</div>
-      <div>{append}</div>
-    </div>
-  );
-}
+import React from "react";
+
+const BannerMessageFC = ({ title, subtitle, prepend, append }) => (
+  <div className="flex flex-col p-16 items-center justify-center text-center">
+    <div>{prepend}</div>
+    <div className="text-4xl text-gray-800 font-bold mb-2">{title}</div>
+    <div className="text-md text-gray-400 mb-4">{subtitle}</div>
+    <div>{append}</div>
+  </div>
+);
+
+export const BannerMessage = React.memo(BannerMessageFC);
