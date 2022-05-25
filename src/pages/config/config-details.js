8a1d1893750eb90b09b2ac3a08eeaca54de4d642
ciju <ciju.ch3rian@gmail.com>
2022-05-25 10:48:01 +0530

    refactor: remove config item history tab from config details page

--- src/pages/config/config-details.js
+++ src/pages/config/config-details.js
@@ -1,15 +1,11 @@
 import { useEffect, useState, useMemo } from "react";
 import toast from "react-hot-toast";
 import { useNavigate, useParams, useSearchParams } from "react-router-dom";
-import { Tab } from "@headlessui/react";
-import clsx from "clsx";
-import { SearchLayout } from "../../components/Layout";
 import { toastError } from "../../components/Toast/toast";
 import { Modal } from "../../components/Modal";
 import { IncidentCreate } from "../../components/Incidents/IncidentCreate";
-import { getConfig, getConfigChange } from "../../api/services/configs";
+import { getConfig } from "../../api/services/configs";
 import { Loading } from "../../components/Loading";
-import { ConfigChangeHistory } from "../../components/ConfigChangeHistory";
 import { JSONViewer } from "../../components/JSONViewer";
 
 export function ConfigDetailsPage() {
@@ -20,17 +16,18 @@ export function ConfigDetailsPage() {
   const [showIncidentModal, setShowIncidentModal] = useState(false);
   const [checked, setChecked] = useState({});
   const [configDetails, setConfigDetails] = useState();
-  const [historyData, setHistoryData] = useState([]);
 
   useEffect(() => {
     getConfig(id)
       .then((res) => {
-        setConfigDetails(res?.data[0]);
+        const data = res?.data[0];
+        setConfigDetails(data);
       })
       .catch((err) => toastError(err))
       .finally(() => {
         setIsLoading(false);
       });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [id]);
 
   useEffect(() => {
@@ -71,24 +68,6 @@ export function ConfigDetailsPage() {
     }
   };
 
-  const onTabChange = (index) => {
-    if (index === 1) {
-      setIsLoading(true);
-      getConfigChange(id)
-        .then((res) => {
-          if (res.data.length === 0) {
-            setHistoryData([]);
-          } else {
-            setHistoryData(res?.data);
-          }
-        })
-        .catch((err) => toastError(err))
-        .finally(() => {
-          setIsLoading(false);
-        });
-    }
-  };
-
   const code = useMemo(
     () =>
       configDetails?.config && JSON.stringify(configDetails.config, null, 2),
@@ -98,141 +77,82 @@ export function ConfigDetailsPage() {
   // TODO(ciju): make this lazy. Only needed for IncidentCreate.
   const configLines = useMemo(() => code && code.split("\n"), [code]);
 
-  const tabs = [
-    {
-      text: "Config",
-      panel: (
-        <div className="flex flex-col w-full border rounded-md rounded-tl-none">
-          {!isLoading ? (
-            <JSONViewer
-              code={code}
-              showLineNo
-              onClick={handleClick}
-              selections={checked}
-            />
-          ) : (
-            <div className="h-32 flex items-center justify-center">
-              <Loading />
-            </div>
-          )}
-        </div>
-      )
-    },
-    {
-      text: "Changes",
-      panel: <ConfigChangeHistory data={historyData} isLoading={isLoading} />
-    }
-  ];
-
   const selectedCount = Object.keys(checked).length;
 
   return (
-    <SearchLayout
-      title={
-        configDetails?.name
-          ? `Config Details for ${configDetails.name}`
-          : "Config Details"
-      }
-    >
-      <div className="flex flex-col items-start">
-        <div className="mb-4 flex flex-row iems-center">
-          <button
-            className="border rounded-md px-3 py-1 text-sm"
-            type="button"
-            onClick={() => navigate("/config")}
-          >
-            Back
-          </button>
-
-          {selectedCount > 0 && (
-            <>
-              <div className="flex items-center mx-4">
-                {selectedCount} lines selected
-              </div>
-              <button
-                className="border rounded-md px-3 py-1 mr-2 text-sm"
-                type="button"
-                onClick={() => {
-                  setChecked({});
-                  setParams({ selected: null });
-                }}
-              >
-                Clear
-              </button>
-              <button
-                className="border rounded-md px-3 py-1 mr-2 text-sm"
-                type="button"
-                onClick={() => {
-                  handleShare();
-                }}
-              >
-                Share
-              </button>
-              <button
-                className="border rounded-md px-3 py-1 text-sm"
-                type="button"
-                onClick={() => setShowIncidentModal(true)}
-              >
-                Create Incident
-              </button>
-            </>
-          )}
-        </div>
-        {/* Tabs */}
-        <Tab.Group onChange={onTabChange}>
-          <Tab.List className=" flex space-x-1 border-gray-300">
-            {tabs.map((tab) => (
-              <Tab
-                key={tab.text}
-                className={({ selected }) =>
-                  clsx(
-                    "rounded-t-md py-2.5 px-4 text-sm leading-5",
-                    "ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400",
-                    selected ? "border border-b-0 border-gray-300" : ""
-                  )
-                }
-              >
-                {tab.text}
-              </Tab>
-            ))}
-          </Tab.List>
-          <Tab.Panels className="">
-            {tabs.map((tab) => (
-              <Tab.Panel
-                key={tab.text}
-                className={clsx(
-                  "rounded-xl bg-white",
-                  "ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400"
-                )}
-              >
-                {tab.panel}
-              </Tab.Panel>
-            ))}
-          </Tab.Panels>
-        </Tab.Group>
-        {/* Modal */}
-        <Modal
-          open={showIncidentModal}
-          onClose={() => setShowIncidentModal(false)}
-          size="small"
-          title="Create New Incident from Selected Evidence"
-        >
-          <IncidentCreate
-            callback={(response) => {
-              navigate(`/incidents/${response.id}`, { replace: true });
-            }}
-            evidence={{
-              configId: id,
-              configName: configDetails?.name,
-              config: configLines,
-              type: "config",
-              lines: Object.fromEntries(
-                Object.keys(checked).map((n) => [n, configLines[n]])
-              )
-            }}
-          />
-        </Modal>
+    <div className="flex flex-col items-start">
+      <div className="mb-4 flex flex-row iems-center">
+        {selectedCount > 0 && (
+          <>
+            <div className="flex items-center mx-4">
+              {selectedCount} lines selected
+            </div>
+            <button
+              className="border rounded-md px-3 py-1 mr-2 text-sm"
+              type="button"
+              onClick={() => {
+                setChecked({});
+                setParams({ selected: null });
+              }}
+            >
+              Clear
+            </button>
+            <button
+              className="border rounded-md px-3 py-1 mr-2 text-sm"
+              type="button"
+              onClick={() => {
+                handleShare();
+              }}
+            >
+              Share
+            </button>
+            <button
+              className="border rounded-md px-3 py-1 text-sm"
+              type="button"
+              onClick={() => setShowIncidentModal(true)}
+            >
+              Create Incident
+            </button>
+          </>
+        )}
       </div>
-    </SearchLayout>
+
+      <div className="flex flex-col w-full border rounded-md rounded-tl-none">
+        {!isLoading ? (
+          <JSONViewer
+            code={code}
+            showLineNo
+            onClick={handleClick}
+            selections={checked}
+          />
+        ) : (
+          <div className="h-32 flex items-center justify-center">
+            <Loading />
+          </div>
+        )}
+      </div>
+
+      <Modal
+        open={showIncidentModal}
+        onClose={() => setShowIncidentModal(false)}
+        size="small"
+        title="Create New Incident from Selected Evidence"
+      >
+        <IncidentCreate
+          callback={(response) => {
+            navigate(`/incidents/${response.id}`, { replace: true });
+          }}
+          evidence={{
+            configId: id,
+            configName: configDetails?.name,
+            config: configLines,
+            type: "config",
+            lines: Object.fromEntries(
+              Object.keys(checked).map((n) => [n, configLines[n]])
+            )
+          }}
+        />
+      </Modal>
+    </div>
   );
 }
