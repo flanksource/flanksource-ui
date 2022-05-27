6a1e2f5e11e949bb697187c4e9f83d6d3c03f564
ciju <ciju.ch3rian@gmail.com>
2022-05-28 03:17:59 +0530

    fix: tailwind and postcss configs

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
-const tailwindcss = require("tailwindcss");
-
 module.exports = {
   // eslint-disable-next-line import/no-extraneous-dependencies, global-require
-  plugins: [tailwindcss("./tailwind.config.js"), require("autoprefixer")]
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {}
+  }
 };
