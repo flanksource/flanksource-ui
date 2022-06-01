b748ff49d04fbae6bbcd0a6efbe607d78ff1ba78
Prashanth-Nelli <micprashanth@gmail.com>
2022-06-01 13:02:30 +0530

    update incidentlist storybooks

--- src/components/Incidents/IncidentList/incidenttList.stories.jsx
+++ src/components/Incidents/IncidentList/incidenttList.stories.jsx
@@ -12,67 +12,71 @@ const incidents = [
     id: "b398da27-7047-407f-a07d-80d0ba168bd0",
     title: "Incident from config",
     created_by: "017fb74a-9c90-2074-6b29-61d71507c231",
-    commander_id: "017fb74a-9c90-2074-6b29-61d71507c231",
-    communicator_id: "017fb74a-9c90-2074-6b29-61d71507c231",
+    commander_id: {
+      name: "Responder 1"
+    },
+    communicator_id: {
+      name: "Responder 2"
+    },
     severity: 2,
     description: "",
     type: "issue",
     status: "open",
     created_at: "2022-04-10T11:32:37.285341",
-    updated_at: "2022-04-10T11:32:37.285341",
-    person: {
-      name: "Responder 1"
-    }
+    updated_at: "2022-04-10T11:32:37.285341"
   },
   {
     id: "749f6387-11a2-4b67-b6dd-625987e6dc60",
     title: "Test Incident 4",
     created_by: "017fb74a-9c90-2074-6b29-61d71507c231",
-    commander_id: "017fb74a-9c90-2074-6b29-61d71507c231",
-    communicator_id: "017fb74a-9c90-2074-6b29-61d71507c231",
+    commander_id: {
+      name: "Responder 3"
+    },
+    communicator_id: {
+      name: "Responder 4"
+    },
     severity: 0,
     description: "test",
     type: "issue",
     status: "open",
     created_at: "2022-04-05T07:55:30.479768",
-    updated_at: "2022-04-05T07:55:30.479768",
-    person: {
-      name: "Responder 1"
-    }
+    updated_at: "2022-04-05T07:55:30.479768"
   },
   {
     id: "9419252b-aea4-4a49-8fff-e2d022fff588",
     title: "Test Incident with hunks",
     created_by: "017fb74a-9c90-2074-6b29-61d71507c231",
-    commander_id: "017fb74a-9c90-2074-6b29-61d71507c231",
-    communicator_id: "017fb74a-9c90-2074-6b29-61d71507c231",
+    commander_id: {
+      name: "Responder 5"
+    },
+    communicator_id: {
+      name: "Responder 6"
+    },
     severity: 0,
     description: "test",
     type: "issue",
     status: "open",
     created_at: "2022-04-05T07:23:11.288297",
-    updated_at: "2022-04-05T07:23:11.288297",
-    person: {
-      name: "Responder 1"
-    }
+    updated_at: "2022-04-05T07:23:11.288297"
   },
   {
     id: "1aa1a99a-b487-495c-9cd9-a13cf717bc10",
     title: "Test incident 3",
     created_by: "017fb74a-9c90-2074-6b29-61d71507c231",
-    commander_id: "017fb74a-9c90-2074-6b29-61d71507c231",
-    communicator_id: "017fb74a-9c90-2074-6b29-61d71507c231",
+    commander_id: {
+      name: "Responder 7"
+    },
+    communicator_id: {
+      name: "Responder 8",
+      avatar:
+        "https://avatars.dicebear.com/v2/female/8e24031284dae52f5e21bc4b3310340b.svg"
+    },
     severity: 0,
     description: "test",
     type: "issue",
     status: "open",
     created_at: "2022-04-05T07:19:03.546871",
-    updated_at: "2022-04-05T07:19:03.546871",
-    person: {
-      name: "Responder 1",
-      avatar:
-        "https://avatars.dicebear.com/v2/female/8e24031284dae52f5e21bc4b3310340b.svg"
-    }
+    updated_at: "2022-04-05T07:19:03.546871"
   }
 ];
 
