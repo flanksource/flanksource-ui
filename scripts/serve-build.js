32458d3fb4047a0b8f5c9d78aea94180c05c85c7
ciju <ciju.ch3rian@gmail.com>
2022-06-01 11:17:03 +0530

    feat: runtime config for routes available on app

--- scripts/serve-build.js
+++ scripts/serve-build.js
@@ -10,11 +10,16 @@ if (process.env.NODE_ENV !== "development") {
   process.exit(1);
 }
 
+const appTypes = ["INCIDENT_MANAGER", "CANARY_CHECKER"];
+
 const staticDir = `${__dirname}/../build`;
 const port = 5050;
 const packagePath = `${__dirname}/../package.json`;
+const indexHTMLPath = `${staticDir}/index.html`;
+const appDeployment = process.env.APP_DEPLOYMENT;
 
 const { proxy: proxyTarget } = JSON.parse(fs.readFileSync(packagePath));
+const indexHTML = fs.readFileSync(indexHTMLPath, { encoding: "utf8" });
 
 const staticServer = new statik.Server(staticDir, { gzip: true });
 
@@ -25,11 +30,16 @@ var proxy = httpProxy.createProxyServer({
 var server = http.createServer((req, res) => {
   req
     .addListener("end", () => {
-      if (!fs.existsSync(`${staticDir}${req.url}`)) {
+      const reqPath = `${staticDir}${req.url}`;
+
+      if (!fs.existsSync(reqPath)) {
         console.info(`Proxying "${req.url}" -> ${proxyTarget}`);
         proxy.web(req, res, {
           target: proxyTarget
         });
+      } else if (req.url == "/" || req.url == "/index.html") {
+        res.write(indexHTML.replace("__APP_DEPLOYMENT__", appDeployment));
+        res.end();
       } else {
         staticServer.serve(req, res);
       }
