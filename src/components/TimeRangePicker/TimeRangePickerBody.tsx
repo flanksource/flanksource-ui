8357b68a19b94f89a348c6f9803209f47fca2cdf
Prashanth-Nelli <micprashanth@gmail.com>
2022-06-24 12:14:32 +0530

    chore: updated button label

--- src/components/TimeRangePicker/TimeRangePickerBody.tsx
+++ src/components/TimeRangePicker/TimeRangePickerBody.tsx
@@ -191,7 +191,7 @@ const TimeRangePickerBodyFC = ({
                 type="button"
                 className="inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-2 py-1 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500"
               >
-                Apply time range
+                Submit
               </button>
             </div>
           </div>
