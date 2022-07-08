94f57a9a46adbba9cfd9a59a16ae2375663c3f4d
Prashanth-Nelli <micprashanth@gmail.com>
2022-07-08 12:28:42 +0530

    feat: add responder response timeline

--- src/components/IncidentDetails/ResponderDetailsDialog/index.tsx
+++ src/components/IncidentDetails/ResponderDetailsDialog/index.tsx
@@ -63,6 +63,13 @@ export function ResponderDetailsDialog({
     return options;
   };
 
+  const getDateDisplayLabel = (date: string) => {
+    if (!date) {
+      return "";
+    }
+    return new Date(date).toLocaleString();
+  };
+
   return (
     // @ts-ignore:next-line
     <Dialog
@@ -75,7 +82,7 @@ export function ResponderDetailsDialog({
       <div className="fixed inset-0 bg-black bg-opacity-25" />
       <div className="fixed inset-0 overflow-y-auto">
         <div className="flex min-h-full items-center justify-center p-4 text-center">
-          <Dialog.Panel className="w-3/6 transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+          <Dialog.Panel className="w-7/12 transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
             <Dialog.Title
               as="h3"
               className="text-lg font-medium leading-6 text-gray-900"
@@ -122,6 +129,185 @@ export function ResponderDetailsDialog({
                     );
                   })}
                 </dl>
+              </div>
+              <div className="lg:border-t lg:border-b lg:border-gray-200 mt-5">
+                <nav
+                  className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8"
+                  aria-label="Progress"
+                >
+                  <ol
+                    role="list"
+                    className="rounded-md overflow-hidden lg:flex lg:border-l lg:border-r lg:border-gray-200 lg:rounded-none"
+                  >
+                    <li className="relative overflow-hidden lg:flex-1">
+                      <div className="border border-gray-200 overflow-hidden border-b-0 rounded-t-md lg:border-0">
+                        <a href="#" className="group">
+                          <span
+                            className="absolute top-0 left-0 w-1 h-full bg-transparent group-hover:bg-gray-200 lg:w-full lg:h-1 lg:bottom-0 lg:top-auto"
+                            aria-hidden="true"
+                          ></span>
+                          <span className="px-2 py-2 flex items-start text-sm font-medium">
+                            <span className="flex-shrink-0">
+                              <span className="w-10 h-10 flex items-center justify-center bg-indigo-600 rounded-full">
+                                <svg
+                                  className="w-6 h-6 text-white"
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  viewBox="0 0 20 20"
+                                  fill="currentColor"
+                                  aria-hidden="true"
+                                >
+                                  <path
+                                    fillRule="evenodd"
+                                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                                    clipRule="evenodd"
+                                  />
+                                </svg>
+                              </span>
+                            </span>
+                            <span className="mt-0.5 ml-4 min-w-0 flex flex-col">
+                              <span className="text-xs font-semibold tracking-wide uppercase">
+                                Created at
+                              </span>
+                              <span className="text-sm font-medium text-gray-500">
+                                {getDateDisplayLabel(
+                                  responder?.json?.created_at
+                                )}
+                              </span>
+                            </span>
+                          </span>
+                        </a>
+                      </div>
+                    </li>
+                    <li className="relative overflow-hidden lg:flex-1">
+                      <div className="border border-gray-200 overflow-hidden lg:border-0">
+                        <a href="#" aria-current="step">
+                          <span
+                            className="absolute top-0 left-0 w-1 h-full bg-indigo-600 lg:w-full lg:h-1 lg:bottom-0 lg:top-auto"
+                            aria-hidden="true"
+                          ></span>
+                          <span className="px-2 py-2 flex items-start text-sm font-medium lg:pl-4">
+                            <span className="flex-shrink-0">
+                              {responder?.json?.acknowledge_time ? (
+                                <span className="w-10 h-10 flex items-center justify-center bg-indigo-600 rounded-full">
+                                  <svg
+                                    className="w-6 h-6 text-white"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 20 20"
+                                    fill="currentColor"
+                                    aria-hidden="true"
+                                  >
+                                    <path
+                                      fillRule="evenodd"
+                                      d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                                      clipRule="evenodd"
+                                    />
+                                  </svg>
+                                </span>
+                              ) : (
+                                <span className="w-10 h-10 flex items-center justify-center border-2 border-indigo-600 rounded-full">
+                                  <span className="text-indigo-600">02</span>
+                                </span>
+                              )}
+                            </span>
+                            <span className="mt-0.5 ml-4 min-w-0 flex flex-col">
+                              <span className="text-xs font-semibold text-indigo-600 tracking-wide uppercase">
+                                Acknowledged at
+                              </span>
+                              <span className="text-sm font-medium text-gray-500">
+                                {getDateDisplayLabel(
+                                  responder?.json?.acknowledge_time
+                                )}
+                              </span>
+                            </span>
+                          </span>
+                        </a>
+
+                        <div
+                          className="hidden absolute top-0 left-0 w-3 inset-0 lg:block"
+                          aria-hidden="true"
+                        >
+                          <svg
+                            className="h-full w-full text-gray-300"
+                            viewBox="0 0 12 82"
+                            fill="none"
+                            preserveAspectRatio="none"
+                          >
+                            <path
+                              d="M0.5 0V31L10.5 41L0.5 51V82"
+                              stroke="currentcolor"
+                              vectorEffect="non-scaling-stroke"
+                            />
+                          </svg>
+                        </div>
+                      </div>
+                    </li>
+
+                    <li className="relative overflow-hidden lg:flex-1">
+                      <div className="border border-gray-200 overflow-hidden border-t-0 rounded-b-md lg:border-0">
+                        <a href="#" className="group">
+                          <span
+                            className="absolute top-0 left-0 w-1 h-full bg-transparent group-hover:bg-gray-200 lg:w-full lg:h-1 lg:bottom-0 lg:top-auto"
+                            aria-hidden="true"
+                          ></span>
+                          <span className="px-2 py-2 flex items-start text-sm font-medium lg:pl-4">
+                            <span className="flex-shrink-0">
+                              {responder?.json?.acknowledge_time &&
+                              responder?.json?.signoff_time ? (
+                                <span className="w-10 h-10 flex items-center justify-center bg-indigo-600 rounded-full">
+                                  <svg
+                                    className="w-6 h-6 text-white"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 20 20"
+                                    fill="currentColor"
+                                    aria-hidden="true"
+                                  >
+                                    <path
+                                      fillRule="evenodd"
+                                      d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                                      clipRule="evenodd"
+                                    />
+                                  </svg>
+                                </span>
+                              ) : (
+                                <span className="w-10 h-10 flex items-center justify-center border-2 border-indigo-600 rounded-full">
+                                  <span className="text-indigo-600">03</span>
+                                </span>
+                              )}
+                            </span>
+                            <span className="mt-0.5 ml-4 min-w-0 flex flex-col">
+                              <span className="text-xs font-semibold text-gray-500 tracking-wide uppercase">
+                                Sign off at
+                              </span>
+                              <span className="text-sm font-medium text-gray-500">
+                                {getDateDisplayLabel(
+                                  responder?.json?.signoff_time
+                                )}
+                              </span>
+                            </span>
+                          </span>
+                        </a>
+
+                        <div
+                          className="hidden absolute top-0 left-0 w-3 inset-0 lg:block"
+                          aria-hidden="true"
+                        >
+                          <svg
+                            className="h-full w-full text-gray-300"
+                            viewBox="0 0 12 82"
+                            fill="none"
+                            preserveAspectRatio="none"
+                          >
+                            <path
+                              d="M0.5 0V31L10.5 41L0.5 51V82"
+                              stroke="currentcolor"
+                              vector-effect="non-scaling-stroke"
+                            />
+                          </svg>
+                        </div>
+                      </div>
+                    </li>
+                  </ol>
+                </nav>
               </div>
             </div>
           </Dialog.Panel>
