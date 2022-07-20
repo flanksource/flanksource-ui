ca47f67f2130f296f7d7e699e7da2d8f433e38cc
ciju <ciju.ch3rian@gmail.com>
2022-07-20 13:11:08 +0530

    refactor: extract HypothesisDeleteDialog component

--- src/components/Hypothesis/HypothesisBar/HypothesisBarDeleteDialog.tsx
+++ src/components/Hypothesis/HypothesisBar/HypothesisBarDeleteDialog.tsx
@@ -1,8 +1,6 @@
-import { Dialog, Transition } from "@headlessui/react";
-import { XIcon } from "@heroicons/react/outline";
-import { Fragment } from "react";
 import { BsTrash } from "react-icons/bs";
 import { IconButton } from "../../IconButton";
+import { HypothesisDeleteDialog } from "../HypothesisDeleteDialog";
 
 interface Props {
   isOpen: boolean;
@@ -38,56 +36,12 @@ export function HypothesisBarDeleteDialog({
         }
       />
 
-      <Dialog
-        open={isOpen}
-        as="div"
-        className="relative z-10"
+      <HypothesisDeleteDialog
+        isOpen={isOpen}
         onClose={onClose}
-      >
-        <div className="fixed inset-0 bg-black bg-opacity-25" />
-
-        <div className="fixed inset-0 overflow-y-auto">
-          <div className="flex min-h-full items-center justify-center p-4 text-center">
-            <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
-              <Dialog.Title
-                as="h3"
-                className="text-lg font-medium leading-6 text-gray-900"
-              >
-                <div className="flex pointer-events-none sm:pointer-events-auto justify-between">
-                  <span>Delete hypothesis? </span>
-                  <button
-                    type="button"
-                    className="text-gray-400 hover:text-gray-500 focus:outline-none"
-                    onClick={onClose}
-                  >
-                    <span className="sr-only">Close</span>
-                    <XIcon className="drop-shadow w-6 h-6" aria-hidden="true" />
-                  </button>
-                </div>
-              </Dialog.Title>
-              <div className="mt-4">
-                <p className="text-sm text-gray-500">
-                  Are you sure you want to delete the hypothesis or would you
-                  rather mark this hypothesis as Disproven.
-                </p>
-              </div>
-
-              <div className="mt-4 space-x-4">
-                <button className="btn-primary" onClick={onDisprove}>
-                  Disprove Hypothesis
-                </button>
-
-                <button
-                  className="btn-secondary btn-secondary-base"
-                  onClick={onDelete}
-                >
-                  Delete Hypothesis
-                </button>
-              </div>
-            </Dialog.Panel>
-          </div>
-        </div>
-      </Dialog>
+        onDelete={onDelete}
+        onDisprove={onDisprove}
+      />
     </>
   );
 }
---
+++ src/components/Hypothesis/HypothesisDeleteDialog/index.tsx
@@ -0,0 +1,64 @@
+import { Dialog } from "@headlessui/react";
+import { XIcon } from "@heroicons/react/outline";
+
+interface IProps {
+  isOpen: boolean;
+  onClose: () => void;
+  onDelete: () => void;
+  onDisprove: () => void;
+}
+
+export function HypothesisDeleteDialog({
+  isOpen,
+  onClose,
+  onDisprove,
+  onDelete
+}: IProps) {
+  return (
+    <Dialog open={isOpen} as="div" className="relative z-10" onClose={onClose}>
+      <div className="fixed inset-0 bg-black bg-opacity-25" />
+
+      <div className="fixed inset-0 overflow-y-auto">
+        <div className="flex min-h-full items-center justify-center p-4 text-center">
+          <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+            <Dialog.Title
+              as="h3"
+              className="text-lg font-medium leading-6 text-gray-900"
+            >
+              <div className="flex pointer-events-none sm:pointer-events-auto justify-between">
+                <span>Delete hypothesis? </span>
+                <button
+                  type="button"
+                  className="text-gray-400 hover:text-gray-500 focus:outline-none"
+                  onClick={onClose}
+                >
+                  <span className="sr-only">Close</span>
+                  <XIcon className="drop-shadow w-6 h-6" aria-hidden="true" />
+                </button>
+              </div>
+            </Dialog.Title>
+            <div className="mt-4">
+              <p className="text-sm text-gray-500">
+                Are you sure you want to delete the hypothesis or would you
+                rather mark this hypothesis as Disproven.
+              </p>
+            </div>
+
+            <div className="mt-4 space-x-4">
+              <button className="btn-primary" onClick={onDisprove}>
+                Disprove Hypothesis
+              </button>
+
+              <button
+                className="btn-secondary btn-secondary-base"
+                onClick={onDelete}
+              >
+                Delete Hypothesis
+              </button>
+            </div>
+          </Dialog.Panel>
+        </div>
+      </div>
+    </Dialog>
+  );
+}
