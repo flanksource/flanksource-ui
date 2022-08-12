0b29ea3ce8a9f222f50be8465b03564fd6ff849e
ciju <ciju.ch3rian@gmail.com>
2022-08-12 12:15:48 +0530

    fix: add missing IconPicker component

---
+++ src/components/IconPicker/index.tsx
@@ -0,0 +1,31 @@
+import { Icons } from "../../icons";
+import { Icon } from "../Icon";
+import { Select } from "../Select";
+
+const iconList = Object.entries(Icons).map(([name, _icon]) => ({
+  value: name,
+  label: (
+    <div className="flex pl-2 space-x-4 items-center">
+      <Icon size="md" name={name} /> <span>{name}</span>
+    </div>
+  )
+}));
+
+interface Props {
+  onChange: (val: { value: string }) => void;
+  icon?: string | undefined;
+}
+export function IconPicker({ onChange, icon }: Props) {
+  const value = iconList.find((x) => x.value === icon);
+
+  return (
+    <div className="w-72">
+      <Select
+        value={value}
+        onChange={onChange}
+        name="Icon"
+        options={iconList}
+      />
+    </div>
+  );
+}
