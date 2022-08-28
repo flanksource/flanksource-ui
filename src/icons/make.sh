6083886a6b76bc9a2f2eb80217bc5dc780b690e3
Moshe Immerman <moshe@flanksource.com>
2022-08-28 10:24:39 +0000

    chore: add more icons

---
+++ src/icons/make.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+	echo "// this file is generated using make index.js" > index.js
+
+
+
+		for img in $(ls *.svg); do
+		name=${img%.*};
+		name=${name//-/_};
+		echo  import  $name from '"'./$img'";'  >> index.js  ;
+    done
+	echo "export const Icons = {" >> index.js
+
+for img in $( ls *.svg); do
+		name=${img%.*};
+		name=${name//-/_};
+		echo  '"'${img%.*}'"': $name,  >> index.js ;
+	done
+
+
+	echo "}" >> index.js
