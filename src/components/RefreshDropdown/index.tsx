508e505adab196cc9ee04a780dc34449118596c3
Maina Wycliffe <wmmaina7@gmail.com>
2022-09-19 16:14:15 +0300

    fix: fix height consistency with other items

--- src/components/RefreshDropdown/index.tsx
+++ src/components/RefreshDropdown/index.tsx
@@ -73,76 +73,78 @@ export default function RefreshDropdown({
   }, [localStorageKey, refreshRate]);
 
   return (
-    <div className="mx-2 relative">
-      <button
-        onClick={() => onClick()}
-        className={` border border-r-0 border-gray-300 p-1 px-2 rounded-md rounded-r-none`}
-      >
-        <HiOutlineRefresh
-          size={18}
-          className={`inline-block ${isLoading && "animate-spin"}`}
-        />
-      </button>
-      <Menu>
-        {/* @ts-expect-error */}
-        <Menu.Button className="border border-gray-300 p-1 rounded-md rounded-l-none">
-          <span className="inline p-1 pr-2"> {refreshRate}</span>
-          <HiOutlineChevronDown className="inline" />
-        </Menu.Button>
-        {/* @ts-expect-error */}
-        <Transition
-          as={Fragment}
-          enter="transition ease-out duration-100"
-          enterFrom="transform opacity-0 scale-95"
-          enterTo="transform opacity-100 scale-100"
-          leave="transition ease-in duration-75"
-          leaveFrom="transform opacity-100 scale-100"
-          leaveTo="transform opacity-0 scale-95"
+    <div className="relative px-2">
+      <div>
+        <button
+          onClick={() => onClick()}
+          className={` border border-r-0 border-gray-300 p-[0.35rem] px-2  rounded-md rounded-r-none`}
         >
+          <HiOutlineRefresh
+            size={18}
+            className={`inline-block ${isLoading && "animate-spin"}`}
+          />
+        </button>
+        <Menu>
           {/* @ts-expect-error */}
-          <Menu.Items
-            as="ul"
-            className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+          <Menu.Button className="border border-gray-300 p-2 text-sm rounded-md rounded-l-none">
+            <span className="inline p-1 pr-2">{refreshRate}</span>
+            <HiOutlineChevronDown className="inline" />
+          </Menu.Button>
+          {/* @ts-expect-error */}
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
           >
-            {RefreshOptionsList.map(([optionsKeys, optionsValue]) => (
-              <>
-                {/* @ts-expect-error */}
-                <Menu.Item
-                  as="li"
-                  className="block"
-                  disabled={
-                    !!refreshDropdownDisabledOptions.find(
-                      (opts) => opts === optionsKeys
-                    )
-                  }
-                >
-                  {({ active, disabled }) => (
-                    <button
-                      onClick={() => setRefreshRate(optionsKeys)}
-                      disabled={
-                        !!refreshDropdownDisabledOptions.find(
-                          (opts) => opts === optionsKeys
-                        )
-                      }
-                      className={`flex group w-full items-center rounded-md px-2 py-2  ${
-                        disabled ? "text-gray-500" : "cursor-pointer"
-                      } ${active ? "bg-blue-200" : "text-gray-900"} ${
-                        refreshRate === optionsKeys ? "font-bold" : ""
-                      }`}
-                    >
-                      <HiOutlineClock
-                        className="mr-2 h-5 w-5"
-                        aria-hidden="true"
-                      />
-                      {optionsValue}
-                    </button>
-                  )}
-                </Menu.Item>
-              </>
-            ))}
-          </Menu.Items>
-        </Transition>
-      </Menu>
+            {/* @ts-expect-error */}
+            <Menu.Items
+              as="ul"
+              className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+            >
+              {RefreshOptionsList.map(([optionsKeys, optionsValue]) => (
+                <>
+                  {/* @ts-expect-error */}
+                  <Menu.Item
+                    as="li"
+                    className="block"
+                    disabled={
+                      !!refreshDropdownDisabledOptions.find(
+                        (opts) => opts === optionsKeys
+                      )
+                    }
+                  >
+                    {({ active, disabled }) => (
+                      <button
+                        onClick={() => setRefreshRate(optionsKeys)}
+                        disabled={
+                          !!refreshDropdownDisabledOptions.find(
+                            (opts) => opts === optionsKeys
+                          )
+                        }
+                        className={`flex group w-full items-center rounded-md px-2 py-2  ${
+                          disabled ? "text-gray-500" : "cursor-pointer"
+                        } ${active ? "bg-blue-200" : "text-gray-900"} ${
+                          refreshRate === optionsKeys ? "font-bold" : ""
+                        }`}
+                      >
+                        <HiOutlineClock
+                          className="mr-2 h-5 w-5"
+                          aria-hidden="true"
+                        />
+                        {optionsValue}
+                      </button>
+                    )}
+                  </Menu.Item>
+                </>
+              ))}
+            </Menu.Items>
+          </Transition>
+        </Menu>
+      </div>
     </div>
   );
 }
