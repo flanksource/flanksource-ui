21b21badfa4d15282f44bd17e285679c4b0c2d97
Moshe Immerman <moshe@flanksource.com>
2022-09-28 09:52:57 +0000

    feat: add currency property type

--- src/components/TopologyCard/CardMetrics.tsx
+++ src/components/TopologyCard/CardMetrics.tsx
@@ -9,33 +9,35 @@ interface IProps {
   row?: boolean;
 }
 
-export const CardMetrics = ({ items, row }: IProps) => (
-  <div className="flex rounded-b-8px divide-x flex-1 justify-between items-center">
-    {items.map((item) => (
-      <div
-        key={item.name}
-        className={clsx(
-          "text-gray-800 px-2 align-middle text-center flex flex-1",
-          {
-            "flex-col": !row,
-            "flex-row items-center justify-center": row
-          }
-        )}
-      >
-        <h6 className="text-gray-color text-xs mb-0.5">
-          {item.name}
-          {row && ":"}
-        </h6>
-        <span
+export const CardMetrics = ({ items, row }: IProps) => {
+  return (
+    <div className="flex rounded-b-8px divide-x flex-1 justify-between items-center">
+      {items.map((item) => (
+        <div
+          key={item.name}
           className={clsx(
-            "font-bold text-xs leading-1.21rel",
-            `text-${item.color}-500`,
-            { "pl-1": row }
+            "text-gray-800 px-2 align-middle text-center h-full  flex flex-1",
+            {
+              "flex-col": !row,
+              "flex-row items-center justify-center": row
+            }
           )}
         >
-          <FormatProperty property={item} short />
-        </span>
-      </div>
-    ))}
-  </div>
-);
+          <h6 className="text-gray-color text-xs mb-0.5">
+            {item.label ?? item.name}
+            {row && ":"}
+          </h6>
+          <span
+            className={clsx(
+              "font-bold text-xs leading-1.21rel flex flex-center justify-center",
+              `text-${item.color}-500`,
+              { "pl-1": row }
+            )}
+          >
+            <FormatProperty property={item} short />
+          </span>
+        </div>
+      ))}
+    </div>
+  );
+};
