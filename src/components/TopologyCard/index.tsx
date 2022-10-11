729cb8949bc13a143796ed464ea567d3f363a82d
Prashanth-Nelli <micprashanth@gmail.com>
2022-10-11 16:01:40 +0530

    feat: add health check summary to topology card

--- src/components/TopologyCard/index.tsx
+++ src/components/TopologyCard/index.tsx
@@ -7,6 +7,7 @@ import { CustomScroll } from "../CustomScroll";
 import { HealthSummary } from "../HealthSummary";
 import { Icon } from "../Icon";
 import { Loading } from "../Loading";
+import { HealthChecksSummary } from "../HealthChecksSummary";
 import { CardMetrics } from "./CardMetrics";
 import { Property } from "./Property";
 import { TopologyDropdownMenu } from "./TopologyDropdownMenu";
@@ -189,7 +190,7 @@ export function TopologyCard({
               className="flex-1 py-4 pl-2 pr-2"
               showMoreClass="text-xs linear-1.21rel mr-1 cursor-pointer"
               maxHeight="200px"
-              minChildCount={6}
+              minChildCount={5}
             >
               {canShowChildHealth() && (
                 <HealthSummary
@@ -198,6 +199,7 @@ export function TopologyCard({
                   component={topology}
                 />
               )}
+              <HealthChecksSummary checks={topology?.checks} />
               {topology?.components?.map((component: any) => (
                 <HealthSummary
                   className="mb-2"
