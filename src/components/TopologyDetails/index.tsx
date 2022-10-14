df06d47a98583243ff02451f1b8f0374f7c7c7de
Maina Wycliffe <wmmaina7@gmail.com>
2022-10-14 10:28:19 +0300

    fix: show all details, whether they have names

--- src/components/TopologyDetails/index.tsx
+++ src/components/TopologyDetails/index.tsx
@@ -11,13 +11,11 @@ type Props = {
 };
 
 export default function TopologyDetails({ topology }: Props) {
-  const filteredTopologyProperties = topology?.properties?.filter(
-    (property) => property.name && property.text
-  );
+  const topologyProperties = topology?.properties ?? [];
 
-  const headline = filteredTopologyProperties?.filter(
-    (property) => property.headline
-  );
+  console.log(topologyProperties);
+
+  const headline = topologyProperties?.filter((property) => property.headline);
 
   return (
     <div className="flex flex-col space-y-2">
@@ -26,7 +24,7 @@ export default function TopologyDetails({ topology }: Props) {
           <div className="flex flex-row divide-x divide-solid space-x-4 px-4 py-6 border border-gray-300 rounded-lg">
             {headline?.map(({ icon, name }) => (
               <div className="flex flex-col w-1/4 space-y-4 items-center justify-center">
-                <Icon name={icon} size="2xsi" />
+                <Icon name={icon} size="2xl" secondary={name} />
                 <label className="font-semibold text-gray-500">{name}</label>
               </div>
             ))}
@@ -37,16 +35,27 @@ export default function TopologyDetails({ topology }: Props) {
         Header={<h3 className="text-xl font-semibold">Details</h3>}
       >
         <div className="flex flex-col px-4">
-          {filteredTopologyProperties &&
-          filteredTopologyProperties?.length > 0 ? (
+          {topologyProperties?.length > 0 ? (
             <table className="table-auto shadow-none">
-              {filteredTopologyProperties
+              {topologyProperties
                 .filter((property) => !property.headline)
                 .map((property) => (
                   <tr>
                     <th className="text-gray-400 font-medium text-left overflow-auto">
-                      {/* @ts-ignore */}
-                      {NodePodPropToLabelMap[property.name] || property.name}:
+                      {property.name ? (
+                        <>
+                          {/* @ts-ignore */}
+                          {NodePodPropToLabelMap[property.name] ||
+                            property.name}
+                          :
+                        </>
+                      ) : (
+                        <Icon
+                          name={property.icon}
+                          secondary={property.name}
+                          size="xs"
+                        />
+                      )}
                     </th>
                     <td className="text-base border-none">
                       <FormatProperty property={property} />
