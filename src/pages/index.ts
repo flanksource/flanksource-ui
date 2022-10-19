d4d113079aa62099cedd39e058785595b7ad6b66
Maina Wycliffe <wmmaina7@gmail.com>
2022-10-19 16:18:28 +0300

    refactor: rename config-list to ConfigList

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,7 +2,7 @@ import { IncidentDetailsPage } from "./incident/IncidentDetails";
 import { IncidentListPage } from "./incident/IncidentListPage";
 import { LogsPage } from "./logs-page";
 import { CanaryPage } from "./canary";
-import { ConfigListPage } from "./config/config-list";
+import { ConfigListPage } from "./config/ConfigList";
 import { ConfigDetailsPage } from "./config/ConfigDetailsPage";
 import { ConfigChangesPage } from "./config/config-changes";
 import { ConfigDetailsChangesPage } from "./config/config-details-changes";
