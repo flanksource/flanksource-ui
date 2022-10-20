e9832c29769246ed62062a6f1e42f32ada4e8a96
Maina Wycliffe <wmmaina7@gmail.com>
2022-10-20 14:51:50 +0300

    refactor: switch _app.js to app.ts

---
+++ pages/_app.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import type { AppProps } from "next/app";
+import "./global.css";
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <div id="root" suppressHydrationWarning>
+      <Component {...pageProps} />
+    </div>
+  );
+}
