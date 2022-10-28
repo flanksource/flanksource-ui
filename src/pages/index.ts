491672ca3e56c2adc29bbc0a6ff6a6ec0d2d24af
Maina Wycliffe <wmmaina7@gmail.com>
2022-10-28 09:25:44 +0300

    refactor: convert logs page to typescript

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,6 +1,6 @@
 import { IncidentDetailsPage } from "./incident/IncidentDetails";
 import { IncidentListPage } from "./incident/IncidentListPage";
-import { LogsPage } from "./logs-page";
+import { LogsPage } from "./LogsPage";
 import { CanaryPage } from "./canary";
 import { ConfigListPage } from "./config/ConfigList";
 import { ConfigDetailsPage } from "./config/ConfigDetailsPage";
