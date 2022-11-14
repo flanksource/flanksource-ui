7fc7193378995bf1e89720b2f49ab380ecf8cf89
Prashanth-Nelli <micprashanth@gmail.com>
2022-11-14 11:31:54 +0530

    chore: sign out action label change

--- src/components/UserProfile/UserProfile.tsx
+++ src/components/UserProfile/UserProfile.tsx
@@ -73,7 +73,7 @@ export function UserProfile() {
                 onClick={onLogout}
                 className="block w-full py-2 px-4 text-left text-sm text-gray-700  hover:bg-gray-50 hover:text-gray-900 border-0 border-b border-gray-200"
               >
-                Sign Out
+                Sign out
               </button>
             </Menu.Item>
           </Menu.Items>
