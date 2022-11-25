c61cef406f69305a8b3ac83c8b418add98d3030c
Benmuiruri <ben.muiruri13@gmail.com>
2022-11-25 07:31:49 +0300

    Add useEffect to log route changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
 import type { AppProps } from "next/app";
 import "./global.css";
 import Router from "next/router";
@@ -11,6 +12,19 @@ Router.events.on("routeChangeError", () => NProgress.done());
 NProgress.configure({ showSpinner: false });
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      console.log(`App is changing to ${url}`);
+    };
+
+    router.events.on("routeChangeStart", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeStart", handleRouteChange);
+    };
+  }, [router]);
+  
   return (
     <div id="root" suppressHydrationWarning>
       <Component {...pageProps} />
