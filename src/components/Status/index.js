e00e82c47c6d43c29298413c4c2424cfdd539e2b
Desmond Obisi <desmondcutest@yahoo.com>
2023-03-04 19:52:29 +0100

    fox: improve the mixed state icon for health page

--- src/components/Status/index.js
+++ src/components/Status/index.js
@@ -3,9 +3,19 @@ import style from "./style.module.css";
 export function Status({ good, mixed, className = "" }) {
   const color = mixed ? style.mixed : good ? "bg-green-400" : "bg-red-400";
   return (
-    <span
-      className={`flex-shrink-0 inline-block h-3 w-3 rounded-full shadow-md ${className} ${color}`}
-      aria-hidden="true"
-    />
+    <>
+      {mixed && (
+        <span
+          className={`flex-shrink-0 inline-block h-1 w-1 ${className} ${color}`}
+          aria-hidden="true"
+        />
+      )}
+      {!mixed && (
+        <span
+          className={`flex-shrink-0 inline-block h-3 w-3 rounded-full mx-0.5 shadow-md ${className} ${color}`}
+          aria-hidden="true"
+        />
+      )}
+    </>
   );
 }
