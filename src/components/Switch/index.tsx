591db1d821237143ba71fd0da3631538b94eb5c4
Maina Wycliffe <wmmaina7@gmail.com>
2023-03-13 23:59:34 +0300

    feat: expand switch component to accept non string options
    feat: expand switch component to accept non string options
    
    feat: expand switch component to accept non string options
    
    fix: add support for boolean for switch component
    
    fix: fix default value issue when it is boolean

--- src/components/Switch/index.tsx
+++ src/components/Switch/index.tsx
@@ -2,9 +2,9 @@ import clsx from "clsx";
 import { useState } from "react";
 
 type Props = {
-  value?: string;
-  onChange?: (value: string) => void;
-  options: string[];
+  value?: string | number | boolean;
+  onChange?: (value: string | number | boolean) => void;
+  options: (number | string | boolean)[];
   className?: string;
 } & Omit<React.DetailsHTMLAttributes<HTMLDivElement>, "onChange">;
 
@@ -15,13 +15,13 @@ export function Switch({
   className,
   ...props
 }: Props) {
-  const [activeOption, setActiveOption] = useState(() => value || options[0]);
+  const [activeOption, setActiveOption] = useState(() => value ?? options[0]);
   const activeClasses =
     "p-1.5 lg:pl-2.5 lg:pr-3.5 rounded-md flex items-center text-sm font-medium bg-white shadow-sm ring-1 ring-black ring-opacity-5";
   const inActiveClasses =
     "p-1.5 lg:pl-2.5 lg:pr-3.5 rounded-md flex items-center text-sm font-medium";
 
-  function handleClick(view: string) {
+  function handleClick(view: string | number | boolean) {
     onChange(view);
     setActiveOption(view);
   }
@@ -41,14 +41,17 @@ export function Switch({
             className="rounded-md flex-1 items-center text-sm text-gray-600 font-medium focus-visible:ring-2 focus-visible:ring-teal-500 focus-visible:ring-offset-2 focus:outline-none focus-visible:ring-offset-gray-100"
             tabIndex={0}
             onClick={(e) => handleClick(option)}
-            key={option}
+            key={option.valueOf().toString()}
           >
             <span
               className={
-                activeOption === option ? activeClasses : inActiveClasses
+                activeOption.valueOf().toString() ===
+                option.valueOf().toString()
+                  ? activeClasses
+                  : inActiveClasses
               }
             >
-              {option}
+              {option.valueOf().toString()}
             </span>
           </button>
         );
