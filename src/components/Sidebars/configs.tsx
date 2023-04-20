0fc5315751ecd0ff3054b1caf08f53731dbc786f
Maina Wycliffe <wmmaina7@gmail.com>
2023-04-20 10:21:58 +0300

    refactor: use react query and fix typescript issues

--- src/components/Sidebars/configs.tsx
+++ src/components/Sidebars/configs.tsx
@@ -1,10 +1,13 @@
 import clsx from "clsx";
 import { useEffect, useState } from "react";
 import { VscJson } from "react-icons/vsc";
-import { reduce } from "lodash";
 import { TbTrash, TbTrashOff } from "react-icons/tb";
 import ReactTooltip from "react-tooltip";
-import { ConfigItem, getConfigsBy } from "../../api/services/configs";
+import {
+  ConfigItem,
+  ConfigTypeRelationships,
+  getConfigsBy
+} from "../../api/services/configs";
 import CollapsiblePanel from "../CollapsiblePanel";
 import ConfigLink from "../ConfigLink/ConfigLink";
 import EmptyState from "../EmptyState";
@@ -12,6 +15,9 @@ import Title from "../Title/title";
 import { Badge } from "../Badge";
 import { IconButton } from "../IconButton";
 import TextSkeletonLoader from "../SkeletonLoader/TextSkeletonLoader";
+import { useQuery } from "@tanstack/react-query";
+import { refreshButtonClickedTrigger } from "../SlidingSideBar";
+import { useAtom } from "jotai";
 
 type Props = {
   topologyId?: string;
@@ -24,42 +30,58 @@ export function ConfigsList({
   configId,
   hideDeletedConfigs
 }: Props) {
-  const [configs, setConfigs] = useState<ConfigItem[]>([]);
-
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    async function fetchConfigs() {
-      setIsLoading(true);
+  const {
+    data: configs = [],
+    isLoading,
+    refetch,
+    isRefetching
+  } = useQuery(
+    [
+      "configs",
+      "related",
+      ...(topologyId ? ["topology", topologyId] : []),
+      ...(configId ? ["configs", configId] : [])
+    ],
+    async () => {
       const res = await getConfigsBy({
         topologyId: topologyId,
         configId: configId
       });
-      let items: ConfigItem[] = [];
-      if (res!.data) {
-        items = reduce(
-          res?.data,
-          (a, i) => {
+      return res?.data ?? [];
+    },
+    {
+      select: (data) => {
+        const items = data
+          .map((item) => ({
+            configs: item.configs,
+            related: item.related
+          }))
+          .reduce((a: ConfigItem[], i) => {
             if (a == null) {
               a = [];
             }
-            if (i.configs?.id !== configId) {
-              a.push(i.configs);
+            if ((i as ConfigTypeRelationships).configs?.id !== configId) {
+              a.push((i as ConfigTypeRelationships).configs);
             }
-            if (i.related?.id !== configId) {
-              a.push(i.related);
+            if ((i as ConfigTypeRelationships).related?.id !== configId) {
+              a.push((i as ConfigTypeRelationships).related);
             }
             return a;
-          },
-          []
-        ).sort((ent: ConfigItem) => (ent.deleted_at ? 1 : -1));
+          }, [])
+          .sort((ent: ConfigItem) => (ent.deleted_at ? 1 : -1));
+        return items ?? [];
       }
-      setConfigs(items);
-      setIsLoading(false);
     }
+  );
 
-    fetchConfigs();
-  }, [topologyId, configId]);
+  const [triggerRefresh] = useAtom(refreshButtonClickedTrigger);
+
+  useEffect(() => {
+    if (!isLoading && !isRefetching) {
+      refetch();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [triggerRefresh]);
 
   return (
     <div className="flex flex-col space-y-4 text-sm">
