07d5567c2b60700fe50ac801346a25a03a50e219
Maina Wycliffe <wmmaina7@gmail.com>
2023-05-10 19:55:26 +0300

    fix: split config type by :: to show duo icons
    Closes #1113

--- src/components/ConfigList/Cells/ConfigListTypeCell.tsx
+++ src/components/ConfigList/Cells/ConfigListTypeCell.tsx
@@ -1,23 +1,43 @@
 import { CellContext } from "@tanstack/react-table";
 import { ConfigItem } from "../../../api/services/configs";
 import { Icon } from "../../Icon";
+import { useMemo } from "react";
 
 export default function ConfigListTypeCell({
-  row,
-  column,
   getValue
 }: CellContext<ConfigItem, unknown>) {
-  const name = row.getIsGrouped()
-    ? row.subRows[0]?.original.type
-    : row.original.type;
-  const secondary = row.getIsGrouped()
-    ? row.subRows[0]?.original.type
-    : row.original.type;
+  const configType = getValue<ConfigItem["type"]>();
+
+  const [primaryIcon, secondaryIcon] = useMemo(() => {
+    if (configType?.split("::").length === 1) {
+      return [configType, undefined];
+    }
+    const primaryIcon = configType?.split("::")[0];
+    const secondaryIcon = configType?.substring(configType.indexOf("::") + 2);
+
+    return [primaryIcon, secondaryIcon];
+  }, [configType]);
+
+  const value = useMemo(() => {
+    if (configType?.split("::").length === 1) {
+      return configType;
+    }
+    return configType
+      ?.substring(configType.indexOf("::") + 2)
+      .replaceAll("::", " ")
+      .trim();
+  }, [configType]);
 
   return (
-    <span className="flex flex-nowrap">
-      <Icon name={name} secondary={secondary} />
-      <span className="pl-1"> {getValue<ConfigItem["type"]>()}</span>
+    <span className="flex flex-nowrap gap-1">
+      <Icon name={primaryIcon} secondary={primaryIcon} className="h-5" />
+      <Icon
+        name={secondaryIcon}
+        secondary={secondaryIcon}
+        className="h-5"
+        prefix={<span className="1">/</span>}
+      />
+      <span className="pl-1"> {value}</span>
     </span>
   );
 }
