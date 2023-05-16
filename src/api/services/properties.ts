363f8baafc68226f3666b06dc8f06fb1e54aeb5b
Prashanth-Nelli <micprashanth@gmail.com>
2023-05-16 18:24:48 +0530

    fix: feature flags table created_by column value fix & styling fixes

--- src/api/services/properties.ts
+++ src/api/services/properties.ts
@@ -1,9 +1,12 @@
+import { AVATAR_INFO } from "../../constants";
 import { Property } from "../../services/permissions/permissionsService";
 import { IncidentCommander } from "../axios";
 import { resolve } from "../resolve";
 
 export const fetchProperties = () => {
-  return resolve<Property[]>(IncidentCommander.get("/properties"));
+  return resolve<Property[]>(
+    IncidentCommander.get(`/properties?select=*,created_by(${AVATAR_INFO})`)
+  );
 };
 
 export const saveProperty = (property: Partial<Property>) => {
