b9556c114e46610515efcc1428d5a8a5962abd20
Maina Wycliffe <wmmaina7@gmail.com>
2023-05-23 22:46:02 +0300

    feat: add tabBy agent to health page

--- src/api/services/topology.ts
+++ src/api/services/topology.ts
@@ -143,6 +143,13 @@ export const getAgentByID = async (id: string) => {
   return res.data?.[0] ?? null;
 };
 
+export const getAgentByIDs = async (ids: string[]) => {
+  const res = await IncidentCommander.get<AgentItem[] | null>(
+    `/agents?select=id,name,description&id=in.(${ids.join(",")})`
+  );
+  return res.data ?? [];
+};
+
 export const getTopologyComponentByID = async (topologyID: string) => {
   const res = await IncidentCommander.get<TopologyComponentItem[]>(
     `/component_names?id=eq.${topologyID}`
