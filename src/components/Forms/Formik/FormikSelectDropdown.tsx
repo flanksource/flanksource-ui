641a6ff4a7790a516cbb44357f1dc308d37d507d
Maina Wycliffe <wmmaina7@gmail.com>
2023-09-05 22:41:21 +0300

    fix: fix dropdown issue with react-select
    chore: update react-select to the latest version
    
    fix: us portals to make react-select behave outside scrolls

---
+++ src/components/Forms/Formik/FormikSelectDropdown.tsx
@@ -0,0 +1,99 @@
+import { useField } from "formik";
+import {
+  ComponentProps,
+  useCallback,
+  useEffect,
+  useMemo,
+  useState
+} from "react";
+import Select from "react-select";
+
+type Option = {
+  label: string;
+  value: string;
+};
+
+type Props = {
+  name: string;
+  label?: string;
+  required?: boolean;
+  hint?: string;
+  options?: Option[];
+} & Omit<ComponentProps<typeof Select>, "option">;
+
+export default function FormikSelectDropdown({
+  name,
+  label,
+  required = false,
+  hint,
+  options,
+  ...props
+}: Props) {
+  const [isTouched, setIsTouched] = useState(false);
+
+  const [field, meta] = useField<string>({
+    name,
+    type: "text",
+    required,
+    validate: useCallback(
+      (value: string) => {
+        if (required && !value) {
+          return "This field is required";
+        }
+      },
+      [required]
+    )
+  });
+
+  const value = useMemo(
+    () => options?.filter((item) => item.value === field.value),
+    [field.value, options]
+  );
+
+  useEffect(() => {
+    setIsTouched(isTouched || meta.touched || meta.initialTouched);
+  }, [isTouched, meta.initialTouched, meta.touched]);
+
+  return (
+    <div className="flex flex-col space-y-2 py-2">
+      {label && (
+        <label className={`block text-sm font-semibold text-gray-700`}>
+          {label}
+        </label>
+      )}
+      <Select
+        name={name}
+        className="h-full shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm border-gray-300 rounded-md"
+        options={options}
+        value={value}
+        onChange={(value: any) => {
+          field.onChange({
+            target: {
+              name: field.name,
+              value: value?.value
+            }
+          });
+        }}
+        menuPortalTarget={document.body}
+        styles={{
+          menuPortal: (base) => ({ ...base, zIndex: 9999 })
+        }}
+        menuPosition={"fixed"}
+        menuShouldBlockScroll={true}
+        onBlur={(event) => {
+          field.onBlur(event);
+          setIsTouched(true);
+        }}
+        onFocus={(event) => {
+          field.onBlur(event);
+          setIsTouched(true);
+        }}
+        {...props}
+      />
+      {hint && <p className="text-sm text-gray-500">{hint}</p>}
+      {isTouched && meta.error ? (
+        <p className="text-sm text-red-500 w-full py-1">{meta.error}</p>
+      ) : null}
+    </div>
+  );
+}
