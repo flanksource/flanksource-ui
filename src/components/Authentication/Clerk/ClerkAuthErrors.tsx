2cdfc31ec2aad27a1543ab984b8977710aa6739e
Maina Wycliffe <wmmaina7@gmail.com>
2023-09-15 09:35:26 +0300

    chore: add org switcher and profile to the error page
    This will allow user to change organization if need be

--- src/components/Authentication/Clerk/ClerkAuthErrors.tsx
+++ src/components/Authentication/Clerk/ClerkAuthErrors.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import { Head } from "../../Head/Head";
 import ErrorPage from "../../Errors/ErrorPage";
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 
 export default function ClerkAuthErrors() {
   const { query } = useRouter();
@@ -11,8 +12,19 @@ export default function ClerkAuthErrors() {
   return (
     <>
       <Head prefix="Bad Session Error" />
-      <div className="flex min-h-screen bg-gray-50 justify-center">
-        <div className="flex min-h-full flex-col justify-center pt-12 pb-28 sm:px-6 lg:px-8">
+      <div className="flex flex-row h-auto w-full bg-gray-50 p-3 space-x-4 items-end border-b border-gray-300">
+        <div className="flex-1 "></div>
+        <div className="h-12 flex flex-row gap-2 items-center">
+          <OrganizationSwitcher
+            hidePersonal
+            createOrganizationMode="modal"
+            afterSwitchOrganizationUrl="/"
+          />
+          <UserButton />
+        </div>
+      </div>
+      <div className="flex flex-col flex-1 bg-gray-50 justify-center">
+        <div className="flex min-h-full flex-col justify-center mx-auto">
           <div className="flex flex-col max-w-screen-lg gap-2 text-center">
             {code === "BAD_SESSION" ? (
               <ErrorPage
