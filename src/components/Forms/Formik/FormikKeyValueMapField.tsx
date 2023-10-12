7a9b1a26cd380e1224ecabdd8887e7e124bd343c
Maina Wycliffe <wmmaina7@gmail.com>
2023-10-12 21:11:30 +0300

    feat: add formik field for adding key values
    fix: fix button for key value formik field
    
    tests: add tests

---
+++ src/components/Forms/Formik/FormikKeyValueMapField.tsx
@@ -0,0 +1,105 @@
+import { useFormikContext } from "formik";
+import { Button } from "../../Button";
+import { TextInput } from "../../TextInput";
+import { get, set } from "lodash";
+import { useEffect, useState } from "react";
+import { FaPlus, FaTrash } from "react-icons/fa";
+
+type LocalStateValue = {
+  key: string;
+  value: string;
+};
+
+type Props = {
+  name: string;
+  label: string;
+};
+
+export default function FormikKeyValueMapField({ name, label }: Props) {
+  const [localValues, setLocalValue] = useState<LocalStateValue[]>([
+    { key: "", value: "" }
+  ]);
+
+  const { values, setFieldValue } =
+    useFormikContext<Record<string, Record<string, string>>>();
+
+  // on mount, set the local values to the formik values and set the formik
+  // values to the local values on change
+  useEffect(() => {
+    const localValues = Object.entries(get(values, name, {})).map(
+      ([key, value]) => ({ key, value })
+    );
+    setLocalValue(localValues);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    const formValue = {};
+    localValues.forEach(({ key, value }) => {
+      set(formValue, key, value);
+    });
+    setFieldValue(name, formValue);
+  }, [localValues, name, setFieldValue]);
+
+  const handleRemove = (index: number) => {
+    setLocalValue((prev) => prev.filter((_, i) => i !== index));
+  };
+
+  const handleAdd = () => {
+    setLocalValue((prev) => [...prev, { key: "", value: "" }]);
+  };
+
+  return (
+    <div className="flex flex-col gap-2">
+      <label className="text-sm font-medium text-gray-700">{label}</label>
+      <div className="flex flex-col gap-2">
+        {localValues.map(({ value, key }, index: number) => (
+          <div className="flex flex-row gap-2" key={index}>
+            <TextInput
+              value={key}
+              onChange={(event) => {
+                const newLocalValues = localValues.map((localValue, i) => {
+                  if (i === index) {
+                    return { ...localValue, key: event.target.value };
+                  }
+                  return localValue;
+                });
+                setLocalValue(newLocalValues);
+              }}
+              id={""}
+            />
+            :
+            <TextInput
+              value={value}
+              onChange={(event) => {
+                // find the key that matches the index
+                const newLocalValues = localValues.map((localValue, i) => {
+                  if (i === index) {
+                    return { ...localValue, value: event.target.value };
+                  }
+                  return localValue;
+                });
+                setLocalValue(newLocalValues);
+              }}
+              id={""}
+            />
+            <Button
+              className="btn-white border-0"
+              onClick={() => handleRemove(index)}
+              aria-label="Remove"
+              icon={<FaTrash />}
+            />
+          </div>
+        ))}
+      </div>
+      <div className="flex flex-row gap-2 justify-start">
+        <Button
+          className="btn-white border-0"
+          onClick={handleAdd}
+          text="Add"
+          icon={<FaPlus />}
+        />
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Forms/Formik/__tests__/FormikKeyValueMapField.unit.test.tsx
@@ -0,0 +1,126 @@
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { Formik, Form } from "formik";
+import FormikKeyValueMapField from "./../FormikKeyValueMapField";
+
+describe("FormikKeyValueMapField", () => {
+  const initialValues = {
+    myMap: {
+      key1: "value1",
+      key2: "value2"
+    }
+  };
+
+  it("renders the label", async () => {
+    render(
+      <Formik initialValues={initialValues} onSubmit={() => {}}>
+        <Form>
+          <FormikKeyValueMapField name="myMap" label="My Map" />
+        </Form>
+      </Formik>
+    );
+    await waitFor(() => {
+      expect(screen.getByText("My Map")).toBeInTheDocument();
+    });
+  });
+
+  it("renders the existing key-value pairs", async () => {
+    render(
+      <Formik initialValues={initialValues} onSubmit={() => {}}>
+        <Form>
+          <FormikKeyValueMapField name="myMap" label="My Map" />
+        </Form>
+      </Formik>
+    );
+    await waitFor(() => {
+      expect(screen.getByDisplayValue("key1")).toBeInTheDocument();
+    });
+    expect(screen.getByDisplayValue("value1")).toBeInTheDocument();
+    expect(screen.getByDisplayValue("key2")).toBeInTheDocument();
+    expect(screen.getByDisplayValue("value2")).toBeInTheDocument();
+  });
+
+  it("adds a new key-value pair when the Add button is clicked", async () => {
+    render(
+      <Formik initialValues={initialValues} onSubmit={() => {}}>
+        <Form>
+          <FormikKeyValueMapField name="myMap" label="My Map" />
+        </Form>
+      </Formik>
+    );
+
+    await expect(
+      screen.findByRole("button", {
+        name: "Add"
+      })
+    ).resolves.toBeInTheDocument();
+    const addButton = screen.getByRole("button", {
+      name: "Add"
+    });
+    userEvent.click(addButton);
+    expect(screen.getByDisplayValue("key1")).toBeInTheDocument();
+    expect(screen.getByDisplayValue("value1")).toBeInTheDocument();
+  });
+
+  it("removes a key-value pair when the Remove button is clicked", async () => {
+    render(
+      <Formik initialValues={initialValues} onSubmit={() => {}}>
+        <Form>
+          <FormikKeyValueMapField name="myMap" label="My Map" />
+        </Form>
+      </Formik>
+    );
+    await expect(screen.findAllByLabelText(/remove/i)).resolves.toHaveLength(2);
+    userEvent.click(screen.getAllByLabelText(/remove/i)[0]);
+
+    await waitFor(() => {
+      expect(screen.queryByDisplayValue("key1")).not.toBeInTheDocument();
+    });
+    await waitFor(() => {
+      expect(screen.queryByDisplayValue("value1")).not.toBeInTheDocument();
+    });
+  });
+
+  it("updates the formik values when a key or value is changed", async () => {
+    const onSubmit = jest.fn();
+    render(
+      <Formik initialValues={initialValues} onSubmit={onSubmit}>
+        <Form>
+          <FormikKeyValueMapField name="myMap" label="My Map" />
+          <button type="submit">Submit</button>
+        </Form>
+      </Formik>
+    );
+
+    await waitFor(() => {
+      expect(screen.getByDisplayValue("key1")).toBeInTheDocument();
+    });
+
+    const keyInput = screen.getByDisplayValue("key1");
+    fireEvent.change(keyInput, {
+      target: {
+        value: "newKey"
+      }
+    });
+    const valueInput = screen.getByDisplayValue("value1");
+    fireEvent.change(valueInput, {
+      target: {
+        value: "newValue"
+      }
+    });
+    const submitButton = screen.getByText("Submit");
+    userEvent.click(submitButton);
+
+    await waitFor(() => {
+      expect(onSubmit).toHaveBeenCalledWith(
+        {
+          myMap: {
+            newKey: "newValue",
+            key2: "value2"
+          }
+        },
+        expect.anything()
+      );
+    });
+  });
+});
