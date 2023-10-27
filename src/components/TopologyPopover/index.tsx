4f03584c3de63fba0dfdc9fd5b3f1ce0aad20603
Maina Wycliffe <wmmaina7@gmail.com>
2023-10-27 09:55:08 +0300

    refactor: move topology sort component into filter component

--- src/components/TopologyPopover/index.tsx
+++ src/components/TopologyPopover/index.tsx
@@ -1,7 +1,5 @@
 import { NavigateOptions, URLSearchParamsInit } from "react-router-dom";
-
 import { TopologyPreference } from "./topologyPreference";
-import { defaultSortLabels, TopologySort } from "./topologySort";
 
 export type SetURLSearchParams = (
   nextInit?:
@@ -10,30 +8,23 @@ export type SetURLSearchParams = (
   navigateOpts?: NavigateOptions
 ) => void;
 
-export const TopologyPopOver = ({
-  size,
-  setSize,
-  sortLabels,
-  searchParams,
-  setSearchParams
-}: {
+type TopologyPopOverProps = {
   size: string;
-  sortLabels: typeof defaultSortLabels;
   setSize: (v: string) => void;
-  searchParams: URLSearchParams;
-  setSearchParams: SetURLSearchParams;
-}) => {
+};
+
+export default function TopologyPopOver({
+  size,
+  setSize
+}: TopologyPopOverProps) {
   const setCardWidth = (width: string) => {
     setSize(`${width}px`);
     localStorage.setItem("topology_card_width", `${width}px`);
   };
 
   return (
-    <div className="relative pt-5 sm:flex md:self-center md:pt-0 pl-3 flex items-center">
-      <TopologySort sortLabels={sortLabels} />
+    <div className="relative pt-5 sm:flex md:self-center md:pt-0 flex items-center">
       <TopologyPreference cardSize={size} setCardWidth={setCardWidth} />
     </div>
   );
-};
-
-export default TopologyPopOver;
+}
