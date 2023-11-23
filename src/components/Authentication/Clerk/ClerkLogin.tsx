735859ba802647ae89e4133cb94cc688dea6eb79
Maina Wycliffe <wmmaina7@gmail.com>
2023-11-23 17:21:27 +0300

    fix: add afterSignUp and afterSigninU URLS to signin and sign up components

--- src/components/Authentication/Clerk/ClerkLogin.tsx
+++ src/components/Authentication/Clerk/ClerkLogin.tsx
@@ -1,5 +1,6 @@
 import { SignIn } from "@clerk/nextjs";
 import { useRouter } from "next/router";
+import { clerkUrls } from "./ClerkAuthSessionChecker";
 
 export default function ClerkLogin() {
   const { query } = useRouter();
@@ -9,6 +10,10 @@ export default function ClerkLogin() {
     : undefined;
 
   return (
-    <SignIn signUpUrl="/registration" afterSignInUrl={redirectUrl ?? "/"} />
+    <SignIn
+      signUpUrl="/registration"
+      afterSignUpUrl={`/${clerkUrls.createOrganization}?return_to=${redirectUrl}`}
+      afterSignInUrl={redirectUrl ?? "/"}
+    />
   );
 }
