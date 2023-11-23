a2989f31aa7e45cddd0fe8740db6ea0f4be7dd2d
Maina Wycliffe <wmmaina7@gmail.com>
2023-11-23 17:22:02 +0300

    fix: if user doesn't have an org created, send to create org, not switcher

--- src/components/Authentication/Clerk/ClerkAuthSessionChecker.tsx
+++ src/components/Authentication/Clerk/ClerkAuthSessionChecker.tsx
@@ -1,4 +1,9 @@
-import { RedirectToSignIn, useOrganization, useSession } from "@clerk/nextjs";
+import {
+  RedirectToSignIn,
+  useOrganization,
+  useOrganizationList,
+  useSession
+} from "@clerk/nextjs";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 import FullPageSkeletonLoader from "../../SkeletonLoader/FullPageSkeletonLoader";
@@ -19,13 +24,33 @@ export default function ClerkAuthSessionChecker({ children }: Props) {
   const { isSignedIn, isLoaded: isSessionLoaded } = useSession();
   const { isLoaded: isOrganizationLoaded, organization } = useOrganization();
 
+  // we need to check if the user has an organization and redirect them to the
+  // create organization page if they do not
+  const { isLoaded: isOrganizationListLoaded, userMemberships } =
+    useOrganizationList();
+
   const { push } = useRouter();
 
   useEffect(() => {
-    if (isOrganizationLoaded && !organization) {
+    if (isOrganizationLoaded && isOrganizationListLoaded && !organization) {
+      // we should redirect to the create organization page if the user is
+      // signed in and does not have an organization
+      if (userMemberships?.count === 0) {
+        push(clerkUrls.createOrganization);
+        return;
+      }
+      // otherwise, we should redirect to the organization switcher page
+      // may be in the future, we should just display the organization switcher
+      // as a modal instead of redirecting
       push(clerkUrls.organizationSwitcher);
     }
-  }, [isOrganizationLoaded, organization, push]);
+  }, [
+    isOrganizationListLoaded,
+    isOrganizationLoaded,
+    organization,
+    push,
+    userMemberships?.count
+  ]);
 
   if (!isOrganizationLoaded || !isSessionLoaded) {
     return <FullPageSkeletonLoader />;
