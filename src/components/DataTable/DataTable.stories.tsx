155404d704f12171ec3bf22592032565c584f015
Maina Wycliffe <wmmaina7@gmail.com>
2023-11-30 19:12:41 +0300

    chore: remove unnecessary file

--- src/components/DataTable/DataTable.stories.tsx
+++ src/components/DataTable/DataTable.stories.tsx
@@ -1,8 +1,52 @@
 import { MemoryRouter } from "react-router-dom";
-import { defaultTableColumns } from "../ConfigViewer/columns";
 import { DataTable } from "./index";
 import React from "react";
 import { ComponentMeta, ComponentStory } from "@storybook/react";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { DateCell } from "../../ui/table/DateCells";
+import { TagsCell } from "../../ui/table/TagCell";
+
+const defaultTableColumns: ColumnDef<any>[] = [
+  {
+    header: "Type",
+    accessorKey: "type",
+    meta: {
+      cellCLassName: "px-5 py-2"
+    }
+  },
+  {
+    header: "Name",
+    accessorKey: "name",
+    meta: {
+      cellCLassName: "px-5 py-2"
+    }
+  },
+  {
+    header: "Tags",
+    accessorKey: "tags",
+    cell: TagsCell,
+    meta: {
+      cellCLassName: "px-5 py-2"
+    }
+  },
+  {
+    header: "Created",
+    accessorKey: "created_at",
+    cell: DateCell,
+    meta: {
+      cellCLassName: "px-5 py-2"
+    }
+  },
+  {
+    header: "Last Updated",
+    accessorKey: "updated_at",
+    cell: DateCell,
+    meta: {
+      cellCLassName: "px-5 py-2"
+    }
+  }
+];
 
 export default {
   title: "DataTable",
