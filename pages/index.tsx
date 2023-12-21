c98cec2791f47bc1a8a5cd4d926b126b65104a6e
Maina Wycliffe <wmmaina7@gmail.com>
2023-12-21 10:47:24 +0300

    fix: fix intercom provider not available

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,16 @@
 import type { NextPage } from "next";
 import { App, CanaryCheckerApp } from "../src/App";
 import AuthSessionChecker from "../src/components/Authentication/AuthSessionChecker";
-import { Head } from "../src/components/Head/Head";
+import SetupIntercom from "../src/components/Intercom/SetupIntercom";
 import { isCanaryUI } from "../src/context/Environment";
 
 const Home: NextPage = () => {
   return (
     <AuthSessionChecker>
       <div className="container-fluid flex flex-col overflow-auto">
-        {isCanaryUI ? <CanaryCheckerApp /> : <App />}
+        <SetupIntercom>
+          {isCanaryUI ? <CanaryCheckerApp /> : <App />}
+        </SetupIntercom>
       </div>
     </AuthSessionChecker>
   );
