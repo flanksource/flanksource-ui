9df086e832d2a39867857b7054de0628166de78e
Maina Wycliffe <wmmaina7@gmail.com>
2023-12-24 00:14:15 +0300

    chore: add global setup for window resizer for jest

--- jest.config.ts
+++ jest.config.ts
@@ -1,16 +1,17 @@
-import nextJest from 'next/jest'
+import nextJest from "next/jest";
 // Sync object
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: './',
-})
+  dir: "./"
+});
 
 // Add any custom config to be passed to Jest
 const customJestConfig = {
   // Add more setup options before each test is run
-  setupFilesAfterEnv: ['@testing-library/jest-dom'], // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
-  moduleDirectories: ['node_modules', '<rootDir>/'],
-  testEnvironment: 'jest-environment-jsdom'
-}
+  setupFilesAfterEnv: ["@testing-library/jest-dom"], // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
+  moduleDirectories: ["node_modules", "<rootDir>/"],
+  testEnvironment: "jest-environment-jsdom",
+  setupFiles: ["./jest.setup.ts"]
+};
 
-module.exports = createJestConfig(customJestConfig)
+module.exports = createJestConfig(customJestConfig);
