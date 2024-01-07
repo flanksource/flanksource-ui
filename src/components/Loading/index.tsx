ad5ee9f22160f3007a176aceaf37f6bf3f946249
Moshe Immermam <moshe@flanksource.com>
2024-01-07 15:08:53 +0200

    feat: improve canary modal loading indicator

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,18 +1,37 @@
 import clsx from "clsx";
-import { Oval } from "react-loading-icons";
+import React from "react";
+
+import { BallTriangle, Oval } from "react-loading-icons";
 
 type Props = {
   text?: string;
+  type?: "circle" | "modal";
 } & React.HTMLAttributes<HTMLDivElement>;
 
-export function Loading({ text = "Loading...", className, ...props }: Props) {
-  return (
-    <div
-      className={clsx("flex justify-center items-center", className)}
-      {...props}
-    >
-      <Oval stroke="gray" height="1.5em" />
-      <span className="text-sm ml-3">{text}</span>
-    </div>
-  );
+export function Loading({
+  text = "Loading...",
+  type = "circle",
+  className,
+  ...props
+}: Props) {
+  if (type === "modal") {
+    return (
+      <div
+        className={clsx("flex justify-center items-center", className)}
+        {...props}
+      >
+        <BallTriangle stroke="zinc" opacity={0.8} fill="gray" height="4em" />
+      </div>
+    );
+  } else {
+    return (
+      <div
+        className={clsx("flex justify-center items-center", className)}
+        {...props}
+      >
+        <Oval stroke="gray" height="1.5em" />
+        <span className="text-sm ml-3">{text}</span>
+      </div>
+    );
+  }
 }
