f5e62ee7ab0e8bcc91b8189591fb45aa69e181af
Maina Wycliffe <wmmaina7@gmail.com>
2024-02-15 22:10:29 +0300

    fix: wrap playbook action results logs
    Fixes #1689

--- src/components/Playbooks/Runs/Actions/PlaybooksActionsResults.tsx
+++ src/components/Playbooks/Runs/Actions/PlaybooksActionsResults.tsx
@@ -3,30 +3,55 @@ import { PlaybookRunAction } from "../../../../api/types/playbooks";
 
 const convert = new Convert();
 
-function DisplayStdout({ stdout }: { stdout?: string }) {
+function DisplayStdout({
+  stdout,
+  className
+}: {
+  stdout?: string;
+  className?: string;
+}) {
   if (!stdout) {
     return null;
   }
   const html = convert.toHtml(stdout);
-  return <pre dangerouslySetInnerHTML={{ __html: html }} />;
+  return (
+    <pre className={className} dangerouslySetInnerHTML={{ __html: html }} />
+  );
 }
 
-function DisplayStderr({ stderr }: { stderr?: string }) {
+function DisplayStderr({
+  stderr,
+  className
+}: {
+  stderr?: string;
+  className?: string;
+}) {
   if (!stderr) {
     return null;
   }
   const html = convert.toHtml(stderr);
   return (
-    <pre className="text-red-500" dangerouslySetInnerHTML={{ __html: html }} />
+    <pre
+      className={`text-red-500 ${className}`}
+      dangerouslySetInnerHTML={{ __html: html }}
+    />
   );
 }
 
-function DisplayLogs({ logs }: { logs?: string }) {
+function DisplayLogs({
+  logs,
+  className
+}: {
+  logs?: string;
+  className?: string;
+}) {
   if (!logs) {
     return null;
   }
   const html = convert.toHtml(logs);
-  return <pre dangerouslySetInnerHTML={{ __html: html }} />;
+  return (
+    <pre className={className} dangerouslySetInnerHTML={{ __html: html }} />
+  );
 }
 
 type Props = {
@@ -51,9 +76,9 @@ export default function PlaybooksRunActionsResults({
   if (result?.stderr || result?.stdout || result?.logs) {
     return (
       <div className={`flex flex-col gap-2 ${className}`}>
-        <DisplayStdout stdout={result?.stdout} />
-        <DisplayStderr stderr={result?.stderr} />
-        <DisplayLogs logs={result?.logs} />
+        <DisplayStdout className={className} stdout={result?.stdout} />
+        <DisplayStderr className={className} stderr={result?.stderr} />
+        <DisplayLogs className={className} logs={result?.logs} />
       </div>
     );
   }
