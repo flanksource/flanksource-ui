f6163ea27f4c1a28671588b1421ca8c1d65df7dd
Moshe Immerman <moshe@flanksource.com>
2024-03-14 11:05:24 +0200

    fix: clerk login

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -24,7 +24,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         >
           {authProvider === "clerk" ? (
             <ClerkProvider
-              domain="flanksource.io"
+              domain="flanksource.com"
               isSatellite={isClerkSatellite}
               {...(isClerkSatellite ? { signInUrl } : {})}
               {...pageProps}
