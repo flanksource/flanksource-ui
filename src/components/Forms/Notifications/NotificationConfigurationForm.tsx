96506ecd4a12d9ce866fed448a4384b6548f5a85
Maina Wycliffe <wmmaina7@gmail.com>
2024-03-21 18:57:13 +0300

    fix: show connection for crd generated notifications
    Fixes #1810

--- src/components/Forms/Notifications/NotificationConfigurationForm.tsx
+++ src/components/Forms/Notifications/NotificationConfigurationForm.tsx
@@ -1,12 +1,24 @@
+import { Notification } from "@flanksource-ui/components/Notifications/notificationsTableColumns";
+import { useFormikContext } from "formik";
 import FormikConnectionField from "../Formik/FormikConnectionField";
+import FormikTextArea from "../Formik/FormikTextArea";
 
 export default function NotificationConfigurationForm() {
+  const { values } = useFormikContext<Notification>();
   return (
     <div className="flex flex-col space-y-2">
-      <FormikConnectionField
-        name={`custom_services.0.connection`}
-        label="Connection"
-      />
+      {values.source === "UI" ? (
+        <FormikConnectionField
+          name={`custom_services.0.connection`}
+          label="Connection"
+        />
+      ) : (
+        <FormikTextArea
+          name={`custom_services.0.connection`}
+          label="Connection"
+          disabled
+        />
+      )}
     </div>
   );
 }
