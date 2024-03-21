e2772bc3e65681b62a855b4ece38c189b96f140c
Maina Wycliffe <wmmaina7@gmail.com>
2024-03-21 18:34:18 +0300

    fix: remove the name and fix missing key to tab

--- src/components/Forms/Notifications/NotificationConfigurationForm.tsx
+++ src/components/Forms/Notifications/NotificationConfigurationForm.tsx
@@ -1,16 +1,8 @@
 import FormikConnectionField from "../Formik/FormikConnectionField";
-import FormikTextInput from "../Formik/FormikTextInput";
 
 export default function NotificationConfigurationForm() {
   return (
     <div className="flex flex-col space-y-2">
-      <FormikTextInput
-        name={`custom_services.0.name`}
-        label="Name"
-        hint="A unique name to identify this notification configuration"
-        required
-      />
-
       <FormikConnectionField
         name={`custom_services.0.connection`}
         label="Connection"
