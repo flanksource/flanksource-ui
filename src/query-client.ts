86d1d4f593d8e90d5f1d956dc5b4456683fc6d94
Maina Wycliffe <wmmaina7@gmail.com>
2024-03-21 16:49:39 +0300

    fix: only retry network and timeout errors, no other errors

--- src/query-client.ts
+++ src/query-client.ts
@@ -1,4 +1,5 @@
 import { QueryClient } from "@tanstack/react-query";
+import { AxiosError } from "axios";
 import { toastError } from "./components/Toast/toast";
 
 const defaultStaleTime = 1000 * 60 * 5;
@@ -10,6 +11,17 @@ export const queryClient = new QueryClient({
       refetchOnMount: true,
       refetchOnReconnect: true,
       staleTime: defaultStaleTime,
+      retry: (failureCount, error) => {
+        if (error instanceof AxiosError) {
+          // Retry timeout errors and network errors with a delay for up to 3 times
+          if (error.code === "ECONNABORTED" || !error.response) {
+            if (failureCount < 3) {
+              return true;
+            }
+          }
+        }
+        return false;
+      },
       onError: (error) => {
         if (error instanceof Error) {
           toastError(`Something went wrong: ${error.message}`);
