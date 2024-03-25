19238afadfe22b73e1439ebb2a5df58ee07f96e8
Maina Wycliffe <wmmaina7@gmail.com>
2024-03-25 14:57:35 +0300

    test: cover more test cases for milicores and bytes input

--- src/components/Forms/Formik/__tests__/FormikBytesTextField.unit.test.tsx
+++ src/components/Forms/Formik/__tests__/FormikBytesTextField.unit.test.tsx
@@ -59,3 +59,38 @@ test("it should submit the form with the correct values", async () => {
     expect(submit).toHaveBeenCalledWith({ bytes: "100Mi" });
   });
 });
+
+test("should have the correct unit selected, according to initial value", async () => {
+  const submit = jest.fn();
+  const initialValues = {
+    bytes: "100Gi"
+  };
+  render(<TestForm onSubmit={submit} initialValues={initialValues} />);
+  const select = screen.getByLabelText("Unit");
+  await waitFor(() => {
+    expect(select).toHaveValue("Gi");
+  });
+});
+
+test("should change the unit when the value is changed", async () => {
+  const submit = jest.fn();
+  render(<TestForm onSubmit={submit} />);
+  const select = screen.getByLabelText("Unit");
+  const input = screen.getByLabelText("Bytes");
+  fireEvent.change(select, { target: { value: "Gi" } });
+  await waitFor(() => {
+    expect(select).toHaveValue("Gi");
+  });
+
+  fireEvent.change(select, { target: { value: "Ti" } });
+  await waitFor(() => {
+    expect(select).toHaveValue("Ti");
+  });
+
+  fireEvent.change(input, { target: { value: "100" } });
+
+  fireEvent.click(screen.getByRole("button", { name: "Submit" }));
+  await waitFor(() => {
+    expect(submit).toHaveBeenCalledWith({ bytes: "100Ti" });
+  });
+});
