508ca1521ceb13edd0010d8c0a68ed2d83ce048e
Maina Wycliffe <wmmaina7@gmail.com>
2024-05-07 20:20:16 +0300

    fix: remove settings related to paste from editor

--- src/ui/CodeEditor/index.tsx
+++ src/ui/CodeEditor/index.tsx
@@ -2,8 +2,7 @@ import Editor, { loader, useMonaco } from "@monaco-editor/react";
 import * as monaco from "monaco-editor";
 import githubLight from "monaco-themes/themes/GitHub Light.json";
 import { configureMonacoYaml } from "monaco-yaml";
-import { useEffect, useRef } from "react";
-import YAML from "yaml";
+import { useEffect } from "react";
 
 loader.config({ monaco });
 
@@ -44,49 +43,9 @@ export function CodeEditor({
   readOnly = false,
   schemaFileName,
   jsonSchemaUrl,
-  language,
-  extractYamlSpecFieldOnPaste = false
+  language
 }: Props) {
-  const editorWrapper = useRef<HTMLDivElement>(null);
-
   const monaco = useMonaco();
-
-  useEffect(() => {
-    if (!extractYamlSpecFieldOnPaste) {
-      return;
-    }
-    // default paste event handler type is just Event, which is inaccurate, hence we
-    // are overriding it here, so we can use ClipboardEvent, which is more
-    // accurate and allows us to access clipboardData safely
-    // @ts-ignore
-    window.addEventListener("paste", (e: ClipboardEvent) => {
-      if (editorWrapper.current?.id === document.activeElement?.id && monaco) {
-        e.preventDefault();
-        const text = e.clipboardData?.getData("text/plain");
-        if (text) {
-          if (language === "yaml") {
-            const yaml = YAML.parse(text);
-            if (yaml.spec) {
-              const model = monaco.editor
-                .getModels()
-                // at the moment, we only have one model that is yaml, but
-                // this may change in the future and we should find a more
-                // robust way of identifying the model and can cause spec being
-                // pasted in the wrong editor
-                .find(
-                  (m) => m.getValue() === text && m.getLanguageId() === "yaml"
-                );
-              model?.setValue(YAML.stringify(yaml.spec));
-            }
-          }
-        }
-      }
-    });
-
-    return () => {
-      window.removeEventListener("paste", () => {});
-    };
-  }, [language, extractYamlSpecFieldOnPaste, monaco]);
 
   // if we have a schema file, we will use the monaco-yaml plugin to enable
   const schemaUrl =
@@ -141,12 +100,7 @@ export function CodeEditor({
         renderLineHighlight: "none",
         readOnly,
         minimap: { enabled: false },
-        formatOnPaste: true,
-        formatOnType: true,
         scrollBeyondLastLine: false
-      }}
-      wrapperProps={{
-        ref: editorWrapper
       }}
       {...{
         // high contrast theme for yaml and json
