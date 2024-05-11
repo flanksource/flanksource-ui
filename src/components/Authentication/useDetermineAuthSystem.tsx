2c9767758fae800496f652cc4a295dfc2e8b58fb
Maina Wycliffe <wmmaina7@gmail.com>
2024-05-11 16:30:12 +0300

    fix: fix return url not working on login page
    - Improve the way we handle the return url on the login page
    - Improve the skeleton loading on the login page

--- src/components/Authentication/useDetermineAuthSystem.tsx
+++ src/components/Authentication/useDetermineAuthSystem.tsx
@@ -1,8 +1,10 @@
 export const isClerkSatellite =
   process.env.NEXT_PUBLIC_CLERK_IS_SATELLITE === "true";
 
+export const isClerkAuthSystem =
+  !!process.env.NEXT_PUBLIC_AUTH_IS_CLERK === true;
+
 export default function useDetermineAuthSystem() {
-  const isClerkAuthSystem = !!process.env.NEXT_PUBLIC_AUTH_IS_CLERK === true;
   if (isClerkAuthSystem) {
     return "clerk";
   }
