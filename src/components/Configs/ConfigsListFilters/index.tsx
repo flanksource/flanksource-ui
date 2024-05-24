52a444c38e11f8665015a1c95a88d1104ff055b0
Maina Wycliffe <wmmaina7@gmail.com>
2024-05-24 09:43:57 +0300

    fix: remove advanced filter option

--- src/components/Configs/ConfigsListFilters/index.tsx
+++ src/components/Configs/ConfigsListFilters/index.tsx
@@ -1,21 +1,13 @@
-import { useConfigPageContext } from "@flanksource-ui/context/ConfigPageContext";
 import { debounce } from "lodash";
-import React, { useMemo, useState } from "react";
+import React from "react";
 import { useSearchParams } from "react-router-dom";
-import { Switch } from "../../Switch";
 import { TextInputClearable } from "../../TextInputClearable";
 import { ConfigListToggledDeletedItems } from "../ConfigListToggledDeletedItems/ConfigListToggledDeletedItems";
-import { QueryBuilder } from "../QueryBuilder";
 import ConfigGroupByDropdown from "./ConfigGroupByDropdown";
 import { ConfigHealthyDropdown } from "./ConfigHealthyDropdown";
 import { ConfigLabelsDropdown } from "./ConfigLabelsDropdown";
 import { ConfigStatusDropdown } from "./ConfigStatusDropdown";
 import { ConfigTypesDropdown } from "./ConfigTypesDropdown";
-
-const ConfigFilterViewTypes = {
-  basic: "Basic",
-  advanced: "Advanced"
-};
 
 function ConfigsListFilterControls() {
   const [params, setParams] = useSearchParams({
@@ -23,66 +15,30 @@ function ConfigsListFilterControls() {
     tag: "All"
   });
 
-  const { setConfigState } = useConfigPageContext();
-
-  const [configFilterView, setConfigFilterView] = useState(() =>
-    params.get("query")
-      ? ConfigFilterViewTypes.advanced
-      : ConfigFilterViewTypes.basic
-  );
-
-  const options = useMemo(() => {
-    return [ConfigFilterViewTypes.basic, ConfigFilterViewTypes.advanced];
-  }, []);
-
   return (
     <div className="flex flex-row items-center gap-2 mr-4">
-      {configFilterView === ConfigFilterViewTypes.advanced ? (
-        <QueryBuilder
-          refreshConfigs={(e: any) => {
-            setConfigState((state) => {
-              return {
-                ...state,
-                data: e
-              };
-            });
-          }}
-        />
-      ) : (
-        <>
-          <ConfigTypesDropdown />
+      <ConfigTypesDropdown />
 
-          <ConfigGroupByDropdown paramsToReset={["tags"]} />
+      <ConfigGroupByDropdown paramsToReset={["tags"]} />
 
-          <ConfigLabelsDropdown />
+      <ConfigLabelsDropdown />
 
-          <ConfigStatusDropdown />
+      <ConfigStatusDropdown />
 
-          <ConfigHealthyDropdown />
+      <ConfigHealthyDropdown />
 
-          <TextInputClearable
-            onChange={debounce((e) => {
-              const query = e.target.value || "";
-              params.set("search", query);
-              setParams(params);
-            }, 200)}
-            className="w-80"
-            placeholder="Search for configs"
-            defaultValue={params.get("search") ?? undefined}
-          />
-
-          <ConfigListToggledDeletedItems />
-        </>
-      )}
-
-      <Switch
-        onChange={(e) => {
-          setConfigFilterView(e as string);
-          setParams({});
-        }}
-        options={options}
-        value={configFilterView}
+      <TextInputClearable
+        onChange={debounce((e) => {
+          const query = e.target.value || "";
+          params.set("search", query);
+          setParams(params);
+        }, 200)}
+        className="w-80"
+        placeholder="Search for configs"
+        defaultValue={params.get("search") ?? undefined}
       />
+
+      <ConfigListToggledDeletedItems />
     </div>
   );
 }
