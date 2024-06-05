1165b6c915329fb1f3fa6e55bc187f54a6bde9fe
Maina Wycliffe <wmmaina7@gmail.com>
2024-06-05 09:44:01 +0300

    fix: incorrect host in agent generation output
    Fixes #1978

---
+++ src/components/Agents/InstalAgentInstruction/useAgentsBaseURL.tsx
@@ -0,0 +1,13 @@
+import useDetermineAuthSystem from "@flanksource-ui/components/Authentication/useDetermineAuthSystem";
+import { useUser } from "@flanksource-ui/context";
+
+export function useAgentsBaseURL() {
+  const { backendUrl } = useUser();
+  const authSystem = useDetermineAuthSystem();
+
+  // if we are on the SaaS platform, we need to use the backend URL from the user
+  // profile, not the current URL
+  const baseUrl = authSystem === "clerk" ? backendUrl : window.location.origin;
+
+  return baseUrl;
+}
