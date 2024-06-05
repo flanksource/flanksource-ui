203cde21c7f30a5fc089e9085c6bab17f2e39574
Maina Wycliffe <wmmaina7@gmail.com>
2024-06-05 09:29:05 +0300

    feat: add percentage property display

--- src/components/Topology/TopologyCard/Property.tsx
+++ src/components/Topology/TopologyCard/Property.tsx
@@ -1,81 +1,9 @@
 import clsx from "clsx";
-import { FiExternalLink } from "react-icons/fi";
 import { Property as PropertyD } from "../../../api/types/topology";
 import { NodePodPropToLabelMap } from "../../../constants";
-import { Age } from "../../../ui/Age";
 import { isEmpty } from "../../Canary/utils";
 import { Icon } from "../../Icon";
-import {
-  FormatPropertyCPUMemory,
-  FormatPropertyCurrency,
-  FormatPropertyDefault,
-  FormatPropertyURL
-} from "./Utils/FormatProperties";
-
-type FormatPropertyProps = {
-  property: PropertyD;
-  short?: boolean;
-  isSidebar?: boolean;
-};
-
-export function FormatProperty({
-  property,
-  short = false,
-  isSidebar = false
-}: FormatPropertyProps) {
-  if (property == null) {
-    return null;
-  }
-
-  let { text } = property;
-
-  if (property.name === "created" && typeof text === "string") {
-    return <Age from={text} />;
-  }
-
-  if (property.type === "url") {
-    return <FormatPropertyURL property={property} short={short} />;
-  }
-
-  if (property.type === "currency") {
-    return <FormatPropertyCurrency property={property} short={short} />;
-  }
-
-  if (property.name === "cpu" || property.name === "memory") {
-    return (
-      <FormatPropertyCPUMemory
-        property={property}
-        isSidebar={isSidebar}
-        short={short}
-      />
-    );
-  }
-
-  if (
-    (property.type === "text" || !property.type) &&
-    !property.value &&
-    property.links
-  ) {
-    return (
-      <div className="flex flex-wrap gap-2">
-        {property.links?.map((link) => (
-          <a
-            key={link.url}
-            href={link.url}
-            className="link"
-            target="_blank"
-            rel="noreferrer"
-          >
-            {link.label}
-            <FiExternalLink className="inline ml-1" size={12} />
-          </a>
-        ))}
-      </div>
-    );
-  }
-
-  return <FormatPropertyDefault property={property} short={short} />;
-}
+import { FormatProperty } from "./FormatProperty";
 
 type PropertyDisplayProps = {
   property: PropertyD;
