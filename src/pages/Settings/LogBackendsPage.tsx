dec9f1bd7eece0d51b64400bed83ac4f33fc3e13
Maina Wycliffe <wmmaina7@gmail.com>
2024-06-18 08:33:03 +0300

    chore: fix failings builds due to duplicate headers

--- src/pages/Settings/LogBackendsPage.tsx
+++ src/pages/Settings/LogBackendsPage.tsx
@@ -1,15 +1,18 @@
+import { getLogsBackends } from "@flanksource-ui/api/schemaResources";
+import ErrorPage from "@flanksource-ui/components/Errors/ErrorPage";
 import { LogBackends } from "@flanksource-ui/components/Logs/LogBackends/LogBackends";
 import LogBackendsForm from "@flanksource-ui/components/Logs/LogBackends/LogBackendsForm";
 import LogBackendsList from "@flanksource-ui/components/Logs/LogBackends/LogBackendsList";
+import {
+  BreadcrumbNav,
+  BreadcrumbRoot
+} from "@flanksource-ui/ui/BreadcrumbNav";
+import { Head } from "@flanksource-ui/ui/Head";
+import { SearchLayout } from "@flanksource-ui/ui/Layout/SearchLayout";
+import { Modal } from "@flanksource-ui/ui/Modal";
 import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 import { AiFillPlusCircle } from "react-icons/ai";
-import { getLogsBackends } from "../../api/schemaResources";
-import ErrorPage from "../../components/Errors/ErrorPage";
-import { BreadcrumbNav, BreadcrumbRoot } from "../../ui/BreadcrumbNav";
-import { Head } from "../../ui/Head";
-import { SearchLayout } from "../../ui/Layout/SearchLayout";
-import { Modal } from "../../ui/Modal";
 
 export function LogBackendsPage() {
   const [isOpen, setIsOpen] = useState(false);
