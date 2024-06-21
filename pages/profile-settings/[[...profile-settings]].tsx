1a88b3e0594d830a6b7a59a3669a08e210bb69a0
Maina Wycliffe <wmmaina7@gmail.com>
2024-06-21 08:30:31 +0300

    fix: fix blank screen after updating the profile settings
    Fixes #2003

--- pages/profile-settings/[[...profile-settings]].tsx
+++ pages/profile-settings/[[...profile-settings]].tsx
@@ -9,7 +9,7 @@ export default function ProfileSettings() {
   return (
     <>
       <Head prefix="Profile Management and Security Settings" />
-      <div className="flex flex-col min-h-screen bg-gray-50 justify-center">
+      <div className="flex flex-col min-h-screen bg-gray-50 justify-center overflow-y-auto">
         <div className="flex min-h-full flex-col justify-center pt-12 pb-28 sm:px-6 lg:px-8">
           {authSystem === "kratos" && <KratosProfileSettings />}
           {authSystem === "clerk" && <ClerkUserProfile />}
