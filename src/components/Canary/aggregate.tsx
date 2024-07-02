f1669406292b95d9f008f34118d121073fc66ac1
Maina Wycliffe <wmmaina7@gmail.com>
2024-07-02 19:24:16 +0300

    refactor: migrate aggregate for canary to typescript

--- src/components/Canary/aggregate.tsx
+++ src/components/Canary/aggregate.tsx
@@ -1,10 +1,11 @@
+import { HealthCheck } from "@flanksource-ui/api/types/health";
 import { reduce } from "lodash";
 import { getPercentage } from "./utils";
 
 // if all check icons are similar in a given iconList, use that icon.
 // if there are different icons, use a icon that indicate 'mixing'.
-function aggregateIcon(iconList) {
-  iconList = iconList.filter((icon) => icon !== "");
+function aggregateIcon(iconList?: string[]) {
+  iconList = (iconList ?? []).filter((icon) => icon !== "");
   if (iconList.length === 0) {
     return null;
   }
@@ -20,7 +21,7 @@ function aggregateIcon(iconList) {
 
 // if all check types are similar in a given typeList, use that type.
 // if there are different types, use a type that indicate 'mixing'.
-function aggregateType(typeList) {
+function aggregateType(typeList: string[]) {
   typeList = typeList.filter((type) => type !== "");
   if (typeList.length === 0) {
     return null;
@@ -37,15 +38,15 @@ function aggregateType(typeList) {
 
 // calculate the average health of all checks with valid statuses
 // returns a simplified list of statuses that indicates the overall health.
-function aggregateStatuses(statusLists) {
+function aggregateStatuses(statusLists?: { status?: string }[]) {
   let allFail = true,
     allPass = true,
     mixed;
-  statusLists.forEach((item) => {
+  statusLists?.forEach((item) => {
     allFail = allFail && item.status === "unhealthy";
   });
 
-  statusLists.forEach((item) => {
+  statusLists?.forEach((item) => {
     allPass = allPass && item.status === "healthy";
   });
 
@@ -59,11 +60,11 @@ function aggregateStatuses(statusLists) {
 
 // The uptime for a group, is defined as the minimum uptime with the group
 // eslint-disable-next-line no-unused-vars
-function minUptime(items) {
+function minUptime(items: { uptime: { passed: number; failed: number } }[]) {
   return reduce(
     items,
-    (old, item) => {
-      if (getPercentage(old.uptime) > getPercentage(item.uptime)) {
+    (old: any, item: any) => {
+      if (getPercentage(old.uptime)! > getPercentage(item.uptime)!) {
         return item;
       }
       return old;
@@ -73,30 +74,30 @@ function minUptime(items) {
 }
 
 // The uptime for a group, is defined as the minimum uptime with the group
-function sumUptime(items) {
+function sumUptime(items: { uptime: { passed: number; failed: number } }[]) {
   return reduce(
     items,
     (old, item) => {
-      old.passed += item.uptime.passed;
-      old.failed += item.uptime.failed;
+      old.passed += item.uptime?.passed;
+      old.failed += item.uptime?.failed;
       return old;
     },
     { passed: 0, failed: 0 }
   );
 }
 
-function avgLatency(items) {
-  const total = reduce(items, (sum, i) => sum + i.latency.rolling1h, 0);
+function avgLatency(items: { latency: { rolling1h: number } }[]) {
+  const total = reduce(items, (sum, i) => sum + i.latency?.rolling1h, 0);
   return total / items.length;
 }
 
-export function aggregate(title, items) {
+export function aggregate(title: string, items: HealthCheck[]) {
   if (items == null) {
     return {};
   }
   return {
     description: title,
-    icon: aggregateIcon(items.map((item) => item.icon)),
+    icon: aggregateIcon(items.map((item) => item.icon!)),
     latency: {
       rolling1h: avgLatency(items)
     },
@@ -111,10 +112,10 @@ export function aggregate(title, items) {
 }
 
 export const getAggregatedGroupedChecks = (
-  groupedChecks,
+  groupedChecks: Record<string, HealthCheck[]> | undefined,
   groupSingleItems = true
 ) => {
-  const aggregatedValue = Object.entries(groupedChecks).reduce(
+  const aggregatedValue = Object.entries(groupedChecks ?? {}).reduce(
     (acc, [k, v]) => {
       if (groupSingleItems || v.length > 1) {
         const aggregated = {
@@ -123,14 +124,14 @@ export const getAggregatedGroupedChecks = (
           subRows: v,
           name: k
         };
-        acc[k] = aggregated;
+        acc[k] = aggregated as any;
       } else if (v.length > 0) {
         const check = v[0];
         acc[k] = check;
       }
       return acc;
     },
-    {}
+    {} as Record<string, HealthCheck>
   );
   return aggregatedValue;
 };
