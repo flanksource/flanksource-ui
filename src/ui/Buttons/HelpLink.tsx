7822ae8bca4031bdf50f016b92d56a2065dbb328
Maina Wycliffe <wmmaina7@gmail.com>
2024-07-10 08:53:21 +0300

    chore: change how url is constructed, to avoid double slashes

--- src/ui/Buttons/HelpLink.tsx
+++ src/ui/Buttons/HelpLink.tsx
@@ -1,10 +1,12 @@
 import { IoMdHelp } from "react-icons/io";
 
 export default function HelpLink({ link }: { link: string }) {
+  const url = new URL(link, "https://docs.flanksource.com/");
+
   return (
     <a
       title="Link to documentation"
-      href={`https://docs.flanksource.com/${link}`}
+      href={url.toString()}
       target="_blank"
       rel="noopener noreferrer"
       className="flex items-center justify-center text-gray-400 hover:text-gray-500 focus:outline-none"
