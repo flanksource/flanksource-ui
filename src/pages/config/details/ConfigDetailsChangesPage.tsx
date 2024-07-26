57a2cabb71621a319f4556bf2fc27cc8e765d099
Maina Wycliffe <wmmaina7@gmail.com>
2024-07-26 09:27:04 +0300

    fix: remove unnecessary extra call to get id,name,config_class,type
    Fixes #2131

--- src/pages/config/details/ConfigDetailsChangesPage.tsx
+++ src/pages/config/details/ConfigDetailsChangesPage.tsx
@@ -21,7 +21,14 @@ export function ConfigDetailsChangesPage() {
     enabled: !!id
   });
 
-  const changes = data?.changes ?? [];
+  const changes = (data?.changes ?? []).map((changes) => ({
+    ...changes,
+    config: {
+      id: changes.config_id!,
+      type: changes.type!,
+      name: changes.name!
+    }
+  }));
 
   const totalChanges = data?.total ?? 0;
   const totalChangesPages = Math.ceil(totalChanges / parseInt(pageSize));
