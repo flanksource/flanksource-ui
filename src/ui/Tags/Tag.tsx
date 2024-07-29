ddd423aa2a7669380b5b124357f8a8738e41a57e
Maina Wycliffe <wmmaina7@gmail.com>
2024-07-29 10:52:09 +0300

    feat: move include/exclude buttons to the tooltip

--- src/ui/Tags/Tag.tsx
+++ src/ui/Tags/Tag.tsx
@@ -27,40 +27,51 @@ export function Tag({
   className = "flex flex-row px-1 py-0.5 rounded-md text-xs whitespace-nowrap break-inside-avoid-column",
   variant = "gray",
   tag,
+  id,
   onFilterByTag,
   ...props
 }: TagProps) {
   return (
     <>
       <div
-        data-tooltip-content={tag ? `${tag.key}: ${tag.value}` : undefined}
-        data-tooltip-id={tag ? `${tag.key}-${tag.value}` : undefined}
-        data-tooltip-class-name="z-[9999]"
+        data-tooltip-id={`${id}-${tag?.key}-${tag?.value}`}
+        data-tooltip-delay-show={100}
         className={clsx(
           className,
           variant === "gray" && "bg-gray-100 text-gray-600",
-          variant === "blue" && "bg-blue-100 text-blue-800",
-          onFilterByTag && "group"
+          variant === "blue" && "bg-blue-100 text-blue-800"
         )}
         {...props}
       >
         {children}
-        {onFilterByTag && tag && (
-          <div className="hidden flex-row gap-1 px-1 group-hover:flex">
-            <IconButton
-              onClick={(e) => onFilterByTag(e, tag, "include")}
-              icon={<PiMagnifyingGlassPlusThin size={18} />}
-              title="Include"
-            />
-            <IconButton
-              onClick={(e) => onFilterByTag(e, tag, "exclude")}
-              icon={<PiMagnifyingGlassMinusThin size={18} />}
-              title="Exclude"
-            />
-          </div>
-        )}
       </div>
-      {tag && <Tooltip id={`${tag.key}-${tag.value}`} />}
+      {tag && (
+        <Tooltip
+          id={`${id}-${tag.key}-${tag.value}`}
+          className="z-[999999] bg-gray-400"
+          clickable
+        >
+          <div className="flex flex-col gap-1">
+            <p className="flex-1">
+              {tag.key}: {tag.value}
+            </p>
+            {onFilterByTag && tag && (
+              <div className="flex flex-row justify-center gap-1 px-1">
+                <IconButton
+                  onClick={(e) => onFilterByTag(e, tag, "include")}
+                  icon={<PiMagnifyingGlassPlusThin size={18} />}
+                  title="Include"
+                />
+                <IconButton
+                  onClick={(e) => onFilterByTag(e, tag, "exclude")}
+                  icon={<PiMagnifyingGlassMinusThin size={18} />}
+                  title="Exclude"
+                />
+              </div>
+            )}
+          </div>
+        </Tooltip>
+      )}
     </>
   );
 }
