c834c2df371b5cb6bcc21e731e14cc4338c3eb10
Maina Wycliffe <wmmaina7@gmail.com>
2024-08-09 07:57:27 +0300

    feat: add feature to download kubeconfig with instructions on how to apply
    fixes #2149

--- src/components/Users/UserProfile.tsx
+++ src/components/Users/UserProfile.tsx
@@ -1,21 +1,44 @@
 import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
+import { useState } from "react";
+import { IoMdDownload } from "react-icons/io";
 import { KratosUserProfileDropdown } from "../Authentication/Kratos/KratosUserProfileDropdown";
 import useDetermineAuthSystem from "../Authentication/useDetermineAuthSystem";
+import AddKubeConfigModal from "../KubeConfig/AddKubeConfigModal";
 
 export function UserProfileDropdown() {
   const authSystem = useDetermineAuthSystem();
+  const [isDownloadKubeConfigModalOpen, setIsDownloadKubeConfigModalOpen] =
+    useState(false);
 
-  return authSystem === "clerk" ? (
-    <div className="flex flex-row gap-2 pr-2">
-      <OrganizationSwitcher
-        hidePersonal
-        createOrganizationMode="modal"
-        afterSelectOrganizationUrl={`/organizations/orgs-switched`}
-        afterCreateOrganizationUrl={`/organizations/orgs-switched`}
+  return (
+    <>
+      {authSystem === "clerk" ? (
+        <div className="flex flex-row gap-2 pr-2">
+          <OrganizationSwitcher
+            hidePersonal
+            createOrganizationMode="modal"
+            afterSelectOrganizationUrl={`/organizations/orgs-switched`}
+            afterCreateOrganizationUrl={`/organizations/orgs-switched`}
+          />
+          <UserButton signInUrl="/login">
+            <UserButton.MenuItems>
+              <UserButton.Action
+                label="Download kubeconfig"
+                labelIcon={<IoMdDownload />}
+                onClick={() => setIsDownloadKubeConfigModalOpen(true)}
+              />
+            </UserButton.MenuItems>
+          </UserButton>
+        </div>
+      ) : (
+        <KratosUserProfileDropdown
+          openKubeConfigModal={() => setIsDownloadKubeConfigModalOpen(true)}
+        />
+      )}
+      <AddKubeConfigModal
+        isOpen={isDownloadKubeConfigModalOpen}
+        onClose={() => setIsDownloadKubeConfigModalOpen(false)}
       />
-      <UserButton signInUrl="/login" />
-    </div>
-  ) : (
-    <KratosUserProfileDropdown />
+    </>
   );
 }
