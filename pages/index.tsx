9e8f42abbfd1f6205921e175e4b7363928ea28a1
Maina Wycliffe <wmmaina7@gmail.com>
2024-09-09 15:18:52 +0300

    fix: fix login screen is show for canary checker image
    Fixes #2267

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,10 +5,19 @@ import AuthSessionChecker from "../src/components/Authentication/AuthSessionChec
 import { isCanaryUI } from "../src/context/Environment";
 
 const Home: NextPage = () => {
+  // For the canary UI, we don't have any authentication system in place
+  if (isCanaryUI) {
+    return (
+      <div className="container-fluid flex flex-col overflow-auto">
+        <CanaryCheckerApp />
+      </div>
+    );
+  }
+
   return (
     <AuthSessionChecker>
       <div className="container-fluid flex flex-col overflow-auto">
-        {isCanaryUI ? <CanaryCheckerApp /> : <App />}
+        <App />
       </div>
     </AuthSessionChecker>
   );
