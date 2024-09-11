a66c033a134d6ca69ede24d2814951b3ecd2a7d6
Maina Wycliffe <wmmaina7@gmail.com>
2024-09-11 18:40:55 +0300

    fix: allow template to be set to default, and custom for code editor
    Fixes #2271

--- src/components/Forms/Formik/FormikNotificationsTemplateField.tsx
+++ src/components/Forms/Formik/FormikNotificationsTemplateField.tsx
@@ -1,3 +1,6 @@
+import { Switch } from "@flanksource-ui/ui/FormControls/Switch";
+import { useField } from "formik";
+import { useState } from "react";
 import { FormikCodeEditor } from "./FormikCodeEditor";
 
 type Props = {
@@ -5,14 +8,46 @@ type Props = {
 };
 
 export default function FormikNotificationsTemplateField({ name }: Props) {
+  const [field] = useField({
+    name
+  });
+
+  const value = field.value;
+
+  const [templateOption, setTemplateOption] = useState<
+    "Default" | "Custom Template"
+  >(() => {
+    return value ? "Custom Template" : "Default";
+  });
+
   return (
     <div className="flex flex-col gap-2">
-      <label className={`form-label`}>Markdown Template</label>
-      <FormikCodeEditor
-        className="flex h-[200px] flex-col"
-        fieldName={name}
-        format={"markdown"}
-      />
+      <label className={`form-label`}>Template</label>
+      <div className="flex w-full flex-row">
+        <Switch
+          options={["Default", "Custom Template"]}
+          className="w-auto"
+          itemsClassName=""
+          defaultValue="Go Template"
+          value={templateOption}
+          onChange={(v) => {
+            setTemplateOption(v);
+            // Clear the field if the user selects the default option
+            if (v === "Default") {
+              field.onChange({
+                target: { value: null, name: field.name }
+              });
+            }
+          }}
+        />
+      </div>
+      {templateOption === "Custom Template" && (
+        <FormikCodeEditor
+          className="flex h-[140px] flex-col"
+          fieldName={name}
+          format={"markdown"}
+        />
+      )}
     </div>
   );
 }
