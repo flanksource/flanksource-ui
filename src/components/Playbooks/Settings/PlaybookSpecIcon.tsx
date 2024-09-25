69c13a34e99c1d985ec3c5b070c76d066aabe4b5
Maina Wycliffe <wmmaina7@gmail.com>
2024-09-25 10:27:30 +0300

    chore: add playbook spec and canary links

--- src/components/Playbooks/Settings/PlaybookSpecIcon.tsx
+++ src/components/Playbooks/Settings/PlaybookSpecIcon.tsx
@@ -1,6 +1,9 @@
+import { getPlaybookSpec } from "@flanksource-ui/api/services/playbooks";
 import { PlaybookSpec } from "@flanksource-ui/api/types/playbooks";
 import { Icon } from "@flanksource-ui/ui/Icons/Icon";
+import TextSkeletonLoader from "@flanksource-ui/ui/SkeletonLoader/TextSkeletonLoader";
 import { Tag } from "@flanksource-ui/ui/Tags/Tag";
+import { useQuery } from "@tanstack/react-query";
 
 type PlaybookIconProps = {
   playbook: Pick<PlaybookSpec, "spec" | "name" | "category" | "title">;
@@ -25,4 +28,19 @@ export default function PlaybookSpecIcon({
       )}
     </div>
   );
+}
+
+export function PlaybookSpecLabel({ playbookId }: { playbookId: string }) {
+  const { data: playbook, isLoading } = useQuery({
+    queryKey: ["playbook", playbookId],
+    queryFn: () => getPlaybookSpec(playbookId),
+    enabled: !!playbookId
+  });
+
+  if (isLoading) {
+    // show skeleton
+    return <TextSkeletonLoader />;
+  }
+
+  return <PlaybookSpecIcon playbook={playbook!} showLabel />;
 }
