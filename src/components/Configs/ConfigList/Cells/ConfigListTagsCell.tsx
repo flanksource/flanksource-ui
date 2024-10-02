be683caeb26a389ddea76c1b213b5178e506e881
Maina Wycliffe <wmmaina7@gmail.com>
2024-10-02 13:04:29 +0300

    refactor: fix failing builds due to typescript errors

--- src/components/Configs/ConfigList/Cells/ConfigListTagsCell.tsx
+++ src/components/Configs/ConfigList/Cells/ConfigListTagsCell.tsx
@@ -4,12 +4,10 @@ import { useCallback } from "react";
 import { useSearchParams } from "react-router-dom";
 import { ConfigItem } from "../../../../api/types/configs";
 
-type ConfigListTagsCellProps<
-  T extends {
-    tags?: Record<string, any>;
-    id: string;
-  }
-> = Pick<CellContext<Pick<T, "tags" | "id">, any>, "getValue" | "row"> & {
+type ConfigListTagsCellProps<T extends Pick<ConfigItem, "tags" | "id">> = Pick<
+  CellContext<Pick<T, "tags" | "id">, any>,
+  "getValue" | "row"
+> & {
   hideGroupByView?: boolean;
   label?: string;
   enableFilterByTag?: boolean;
