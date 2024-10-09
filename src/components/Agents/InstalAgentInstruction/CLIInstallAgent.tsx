edc30b7ecd2154665f5fdccbbc5dc0203f812cb9
Maina Wycliffe <wmmaina7@gmail.com>
2024-10-09 10:44:37 +0300

    fix: agent install instructions
    Update FluxInstallAgent.tsx
    
    Update CLIInstallAgent.tsx
    
    Update CLIInstallAgent.unit.test.tsx
    
    Update FluxInstallAgent.unit.test.tsx
    
    Update CLIInstallAgent.tsx
    
    Update CLIInstallAgent.unit.test.tsx
    
    refactor: lift up the data composition to parent component
    
    chore: fix failing tests
    
    chore: fix issue after running npm i
    
    fix: make the values being passed to helm & flux template consistent and external
    
    fix: fix  issues

--- src/components/Agents/InstalAgentInstruction/CLIInstallAgent.tsx
+++ src/components/Agents/InstalAgentInstruction/CLIInstallAgent.tsx
@@ -1,76 +1,38 @@
-import { GeneratedAgent } from "@flanksource-ui/api/services/agents";
-import { useUser } from "@flanksource-ui/context";
 import CodeBlock from "@flanksource-ui/ui/Code/CodeBlock";
 import Handlebars from "handlebars";
 import { useMemo } from "react";
-import { AgentFormValues } from "../Add/AddAgentForm";
-import { useAgentsBaseURL } from "./useAgentsBaseURL";
+import { TemplateContextData } from "./InstallAgentModal";
 
-const helmCommand = `helm repo add flanksource https://flanksource.github.io/charts
+// This a  Handlebars template for the Helm command to install the agent and the
+// kubernetes agent if the user has enabled it.
+const helmCommand = `helm repo add {{ chart }} {{ repoName }}/{{ chart }}
 
 helm repo update
 
-helm install mc-agent flanksource/mission-control-agent -n "mission-control-agent" \\
-  --set upstream.createSecret=true \\
-  --set upstream.host={{baseUrl}} \\
-  --set upstream.username=token \\
-  --set upstream.password={{generatedAgent.access_token}} \\
-  --set upstream.agentName={{agentFormValues.name}} \\
-{{#if pushTelemetry}}
-  --set pushTelemetry.enabled=true \\
-  --set pushTelemetry.topologyName={{pushTelemetry.topologyName}}
-{{/if}}
+helm install mc-agent flanksource/mission-control-agent -n "{{{ namespace }}}" \\
+  {{#each values}}
+  --set {{{ this }}} \\
+  {{/each}}
   --create-namespace
 
-{{#if kubeOptions}}
-helm install mc-agent-kubernetes flanksource/mission-control-kubernetes -n "mission-control-agent"  \\
-  --set scraper.clusterName="{{agentFormValues.name}}" \\
-  --set scraper.interval="{{kubeOptions.interval}}"
+{{#if kubeValues }}
+helm install mc-agent-kubernetes flanksource/mission-control-kubernetes -n "{{{ namespace }}}"  \\
+  {{#each kubeValues}}
+  --set {{{ this }}} \\
+  {{/each}}
 {{/if}}
 `;
 
 const template = Handlebars.compile(helmCommand);
 
 type Props = {
-  generatedAgent: GeneratedAgent;
-  agentFormValues?: AgentFormValues;
+  data: TemplateContextData;
 };
 
-export default function CLIInstallAgent({
-  generatedAgent,
-  agentFormValues
-}: Props) {
-  const baseUrl = useAgentsBaseURL();
-  const { backendUrl, orgSlug } = useUser();
-
+export default function CLIInstallAgent({ data }: Props) {
   const helmCommandTemplate = useMemo(() => {
-    const kubeOptions = agentFormValues?.kubernetes;
-    const pushTelemetry = agentFormValues?.pushTelemetry;
-
-    return template(
-      {
-        generatedAgent,
-        baseUrl,
-        agentFormValues,
-        pushTelemetry: pushTelemetry?.enabled
-          ? {
-              ...pushTelemetry,
-              topologyName: orgSlug
-                ? `${orgSlug}-${pushTelemetry.topologyName}`
-                : pushTelemetry.topologyName
-            }
-          : undefined,
-        backendUrl,
-        kubeOptions: kubeOptions
-          ? {
-              interval: kubeOptions?.interval,
-              exclusions: `{${kubeOptions?.exclusions?.join(",")}}`
-            }
-          : undefined
-      },
-      {}
-    );
-  }, [agentFormValues, backendUrl, baseUrl, generatedAgent, orgSlug]);
+    return template(data, {});
+  }, [data]);
 
   return (
     <div className="flex flex-1 flex-col gap-4 overflow-y-auto p-2">
