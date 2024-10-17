4f3e4742e23671fd6e775b2e1e33db67bdf57a56
Maina Wycliffe <wmmaina7@gmail.com>
2024-10-17 10:02:02 +0300

    feat: add notification rule name to the ui
    Fixes #2349

--- src/components/Notifications/Rules/NotificationsRulesForm.tsx
+++ src/components/Notifications/Rules/NotificationsRulesForm.tsx
@@ -44,6 +44,7 @@ export default function NotificationsRulesForm({
             className="flex flex-1 flex-col gap-4 overflow-y-auto"
           >
             <div className="flex flex-col gap-4 overflow-y-auto p-4">
+              <FormikTextInput name="name" label="Name" />
               <FormikTextInput name="title" label="Title" />
               <FormikNotificationEventsDropdown
                 name="events"
