a178e8d32ba3e4d309e4363dcfa89de78fbb5885
Maina Wycliffe <wmmaina7@gmail.com>
2024-10-30 20:20:02 +0300

    fix: remember selected catalog tab when navigating to `/catalog/{id}`
    Fixes #2360

--- src/components/Configs/ConfigDetailsTabs.tsx
+++ src/components/Configs/ConfigDetailsTabs.tsx
@@ -1,3 +1,4 @@
+import useNavigateToPreviousVisitedCatalogTab from "@flanksource-ui/hooks/useNavigateToPreviousVisitedCatalogTab";
 import { SearchLayout } from "@flanksource-ui/ui/Layout/SearchLayout";
 import clsx from "clsx";
 import { useAtom } from "jotai";
@@ -36,6 +37,8 @@ export function ConfigDetailsTabs({
   className = "p-2"
 }: ConfigDetailsTabsProps) {
   const { id } = useParams();
+
+  useNavigateToPreviousVisitedCatalogTab();
 
   const [, setRefreshButtonClickedTrigger] = useAtom(
     refreshButtonClickedTrigger
