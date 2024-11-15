31ccfa7c9f4751cf36d61d7b077f285199d497da
Moshe Immermam <moshe@flanksource.com>
2024-11-15 11:14:27 +0200

    chore: fix notification rules table

--- src/components/Notifications/Rules/NotificationsRulesTable.tsx
+++ src/components/Notifications/Rules/NotificationsRulesTable.tsx
@@ -1,14 +1,9 @@
 import { NotificationRules } from "@flanksource-ui/api/types/notifications";
-import { Modal } from "@flanksource-ui/ui/Modal";
 import MRTDataTable from "@flanksource-ui/ui/MRTDataTable/MRTDataTable";
-import { useAtom } from "jotai";
 import { useCallback } from "react";
 import { useSearchParams } from "react-router-dom";
 import EditNotificationRules from "./EditNotificationRules";
-import {
-  notificationMostCommonErrorAtom,
-  notificationsRulesTableColumns
-} from "./notificationsRulesTableColumns";
+import { notificationsRulesTableColumns } from "./notificationsRulesTableColumns";
 
 type NotificationsTableProps = {
   notifications: NotificationRules[];
@@ -29,14 +24,6 @@ export default function NotificationsRulesTable({
 
   const selectedNotificationId = searchParams.get("id");
 
-  const [mostCommonErrorNotification, setMostCommonErrorNotification] = useAtom(
-    notificationMostCommonErrorAtom
-  );
-  const modalTitle =
-    mostCommonErrorNotification?.title ??
-    mostCommonErrorNotification?.person?.name ??
-    mostCommonErrorNotification?.team?.name;
-
   const onSelectNotification = useCallback(
     (notification: NotificationRules) => {
       const id = notification.id;
@@ -48,17 +35,6 @@ export default function NotificationsRulesTable({
 
   return (
     <>
-      {mostCommonErrorNotification && (
-        <Modal
-          open={mostCommonErrorNotification !== undefined}
-          onClose={() => setMostCommonErrorNotification(undefined)}
-          title={`${modalTitle ?? "Most Common Error"}`}
-        >
-          <div className="flex flex-col p-4">
-            {mostCommonErrorNotification.most_common_error}
-          </div>
-        </Modal>
-      )}
       <MRTDataTable
         data={notifications ?? []}
         columns={notificationsRulesTableColumns}
