ca9eaaa3f6303b57087ec6b797c97c4b0c306108
Aditya Thebe <contact@adityathebe.com>
2025-02-20 15:27:42 +0545

    feat: Help link on playbooks resource selector

--- src/ui/Buttons/HelpLink.tsx
+++ src/ui/Buttons/HelpLink.tsx
@@ -1,17 +1,38 @@
-import { IoMdHelp } from "react-icons/io";
+import { QuestionMarkCircleIcon } from "@heroicons/react/solid";
 
-export default function HelpLink({ link }: { link: string }) {
+type HelpLinkProps = {
+  link: string;
+  iconID?: string;
+  title?: string;
+  className?: string;
+};
+
+export default function HelpLink({
+  link,
+  title,
+  iconID,
+  className
+}: HelpLinkProps) {
   const url = new URL(link, "https://docs.flanksource.com/");
+  const defaultClass =
+    "flex items-center justify-center text-gray-400 hover:text-gray-500 focus:outline-none";
+  const classes = className ? className : defaultClass;
+  if (title === undefined) {
+    title = "Link to documentation";
+  }
 
   return (
     <a
-      title="Link to documentation"
+      title={title}
       href={url.toString()}
       target="_blank"
       rel="noopener noreferrer"
-      className="flex items-center justify-center text-gray-400 hover:text-gray-500 focus:outline-none"
+      className={classes}
     >
-      <IoMdHelp size={22} className="inline-block" />
+      <QuestionMarkCircleIcon
+        id={iconID}
+        className="ml-1 inline-block h-4 w-4 text-current"
+      />
     </a>
   );
 }
