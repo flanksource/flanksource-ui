287f1780e53417adc49e890fb0fb5d92d28dd0ff
Aditya Thebe <contact@adityathebe.com>
2025-03-29 21:49:57 +0545

    feat: notification resource search

--- src/components/Notifications/Filters/NotificationFilterBar.tsx
+++ src/components/Notifications/Filters/NotificationFilterBar.tsx
@@ -1,4 +1,5 @@
 import FormikFilterForm from "../../Forms/FormikFilterForm";
+import FormikSearchInputClearable from "../../Forms/Formik/FormikSearchInputClearable";
 import NotificationResourceTypeDropdown from "./NotificationResourceTypeDropdown";
 import NotificationStatusDropdown from "./NotificationStatusDropdown";
 
@@ -6,11 +7,15 @@ export default function NotificationFilterBar() {
   return (
     <FormikFilterForm
       paramsToReset={["pageIndex", "pageSize"]}
-      filterFields={["status", "resource_type"]}
+      filterFields={["status", "resource_type", "search"]}
     >
       <div className="flex flex-row gap-2 py-3">
         <NotificationStatusDropdown />
         <NotificationResourceTypeDropdown />
+        <FormikSearchInputClearable
+          name="search"
+          placeholder="Search by resources"
+        />
       </div>
     </FormikFilterForm>
   );
