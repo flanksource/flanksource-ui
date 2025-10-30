2b20970e2d21c0937832ae476a423cad842320b0
Aditya Thebe <contact@adityathebe.com>
2025-10-30 15:52:38 +0545

    Add BarGauge panel type for multi-bar visualizations

--- src/pages/audit-report/components/View/panels/index.ts
+++ src/pages/audit-report/components/View/panels/index.ts
@@ -2,5 +2,6 @@ export { default as NumberPanel } from "./NumberPanel";
 export { default as TablePanel } from "./TablePanel";
 export { default as PieChartPanel } from "./PieChartPanel";
 export { default as GaugePanel } from "./GaugePanel";
+export { default as BarGaugePanel } from "./BarGaugePanel";
 export { default as TextPanel } from "./TextPanel";
 export { default as DurationPanel } from "./DurationPanel";
