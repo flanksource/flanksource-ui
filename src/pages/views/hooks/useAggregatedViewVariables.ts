52cce0243349e5af66722ba2c08f9c8b2bf7ef28
Aditya Thebe <contact@adityathebe.com>
2025-12-01 23:08:22 +0545

    fix: updating the dependent templating variable

--- src/pages/views/hooks/useAggregatedViewVariables.ts
+++ src/pages/views/hooks/useAggregatedViewVariables.ts
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import { useQueries } from "@tanstack/react-query";
 import { getViewDataByNamespace } from "../../../api/services/views";
 import { aggregateVariables } from "../utils/aggregateVariables";
@@ -11,13 +12,33 @@ interface ViewRef {
 
 export function useAggregatedViewVariables(sections: ViewRef[]) {
   const [viewVarParams] = usePrefixedSearchParams(VIEW_VAR_PREFIX, false);
-  const currentVariables = Object.fromEntries(viewVarParams.entries());
+  const viewVarParamsString = useMemo(
+    () => viewVarParams.toString(),
+    [viewVarParams]
+  );
+  const currentVariables = useMemo(
+    () => Object.fromEntries(viewVarParams.entries()),
+    [viewVarParams]
+  );
+
+  // Use a stable key for the current variables to avoid needless refetches
+  const currentVariablesKey = viewVarParamsString;
 
   // Fetch all sections in parallel
   const queries = useQueries({
     queries: sections.map((section) => ({
-      queryKey: ["view-variables", section.namespace, section.name],
-      queryFn: () => getViewDataByNamespace(section.namespace, section.name),
+      queryKey: [
+        "view-variables",
+        section.namespace,
+        section.name,
+        currentVariablesKey
+      ],
+      queryFn: () =>
+        getViewDataByNamespace(
+          section.namespace,
+          section.name,
+          currentVariables
+        ),
       enabled: !!section.namespace && !!section.name,
       staleTime: 5 * 60 * 1000,
       keepPreviousData: true
