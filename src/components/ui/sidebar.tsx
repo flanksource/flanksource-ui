cc3ce56446247df92541d78df697336dd14c162a
Yash Mehrotra <yashmehrotra95@gmail.com>
2025-12-05 19:35:47 +0530

    chore: cleanup

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -734,8 +734,6 @@ const SidebarMenuSubButton = React.forwardRef<
       className={cn(
         "flex h-6 min-w-0 -translate-x-px items-center gap-1.5 overflow-hidden rounded-md px-1.5 text-sidebar-foreground outline-none ring-sidebar-ring hover:bg-sidebar-accent hover:text-sidebar-accent-foreground focus-visible:ring-2 active:bg-sidebar-accent active:text-sidebar-accent-foreground disabled:pointer-events-none disabled:opacity-50 aria-disabled:pointer-events-none aria-disabled:opacity-50 [&>span:last-child]:truncate [&>svg]:size-3.5 [&>svg]:shrink-0 [&>svg]:text-sidebar-accent-foreground",
         "data-[active=true]:bg-sidebar-accent data-[active=true]:text-sidebar-accent-foreground",
-        size === "sm" && "",
-        size === "md" && "",
         "group-data-[collapsible=icon]:hidden",
         className
       )}
