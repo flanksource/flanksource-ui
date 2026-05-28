da1a920b62c2fcc7be785c6dcd920bcb49cdbd09
Yash Mehrotra <yashmehrotra95@gmail.com>
2026-05-28 18:40:04 +0530

    chore: fix clerk detection system

--- src/components/Authentication/useDetermineAuthSystem.tsx
+++ src/components/Authentication/useDetermineAuthSystem.tsx
@@ -2,7 +2,7 @@ export const isClerkSatellite =
   process.env.NEXT_PUBLIC_CLERK_IS_SATELLITE === "true";
 
 export const isClerkAuthSystem =
-  !!process.env.NEXT_PUBLIC_AUTH_IS_CLERK === true;
+  process.env.NEXT_PUBLIC_AUTH_IS_CLERK === "true";
 
 export const isBasicAuthSystem =
   process.env.NEXT_PUBLIC_AUTH_IS_BASIC === "true";
